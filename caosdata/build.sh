64ae2ca8a37073a487b2f4386e4069c42277427e
Alyssa Milburn <fuzzie@fuzzie.org>
2004-06-08 13:04:44 +0000

    more build system work. going to shower now\!

--- caosdata/build.sh
+++ caosdata/build.sh
@@ -2,3 +2,10 @@
 
 ./makedocs.pl
 cp caosdata.cpp ..
+cd hash
+make
+cd ..
+hash/generatehex b prehash_cmds.txt > prehash_cmds_b.txt
+hash/generatehex b prehash_funcs.txt > prehash_funcs_b.txt
+hash/generatehex l prehash_cmds.txt > prehash_cmds_l.txt
+hash/generatehex l prehash_funcs.txt > prehash_funcs_l.txt
