cc2b941b84a3fdc4692dc723677d9b90600cc5a7
Alyssa Milburn <fuzzie@fuzzie.org>
2004-06-12 17:49:52 +0000

    add copyright notices to new files

--- agentfile.h
+++ agentfile.h
@@ -1,3 +1,22 @@
+/*
+ *  agentfile.h
+ *  openc2e
+ *
+ *  Created by Alyssa Milburn on Tue Aug 28 2001.
+ *  Copyright (c) 2001,2004 Alyssa Milburn. All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ */
+
 #include <vector>
 #include <string>
 #include <istream>
