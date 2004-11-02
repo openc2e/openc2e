4108c84e8dea0c3b5e40ec2187dd37b28e1ec5f8
Alyssa Milburn <fuzzie@fuzzie.org>
2004-11-02 20:05:21 +0000

    add command-line options to praydumper

--- tools/praydumper.cpp
+++ tools/praydumper.cpp
@@ -1,14 +1,26 @@
 #include "agentfile.h"
 #include <iostream>
 #include <fstream>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
+	bool outputfiles = false, notags = false, usageerror = false;
 	cerr << "praydumper, part of openc2e, copyright (c) 2004 Alyssa Milburn" << endl << endl;
-	if (argc != 2) {
-		cerr << "syntax: praydumper filename" << endl;
+	int ch;
+	while ((ch = getopt(argc, argv, "on")) != -1) {
+		switch (ch) {
+			case 'o': outputfiles = true; break;
+			case 'n': notags = true; break;
+			default: usageerror = true; break;
+		}
+	}
+	argc -= optind;
+	argv += optind;
+	if ((argc != 1) || usageerror) {
+		cerr << "syntax: praydumper -o -n filename\n-o output uninterpreted blocks as files (currently unimplemented)\n-n do not interpret tag blocks" << endl;
 		return 1;
 	}
-	ifstream input(argv[1]);
+	ifstream input(argv[0]);
 	// todo: check input is valid
 	cout << "(- praydumper-generated PRAY file from '" << argv[1] << "' -)" << endl;
 	cout << endl << "\"en-GB\"" << endl;
@@ -19,7 +31,7 @@ int main(int argc, char **argv) {
 	for (vector<block *>::iterator x = file.blocks.begin(); x != file.blocks.end(); x++) {
 		tagPrayBlock *b = dynamic_cast<tagPrayBlock *>(*x);
 
-		if (b) {
+		if (!notags && b) {
 			cout << endl << "group " << (*x)->blockname() << " \"" << (*x)->name() << "\"" << endl;
 			for (vector<pair<string, unsigned int> >::iterator y = b->intvalues.begin(); y != b->intvalues.end(); y++) {
 				cout << "\"" << y->first << "\" " << y->second << endl;
