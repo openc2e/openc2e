bb39f7fd57e8927bf419534cfb1e57c45bf098d4
Alyssa Milburn <fuzzie@fuzzie.org>
2004-11-16 22:15:24 +0000

    add Bertram's mngfile class to the codebase

---
+++ mngfile.cpp
@@ -0,0 +1,94 @@
+#include <assert.h>
+#include "endianlove.h"
+#include <stdio.h>
+#include "mngfile.h"
+#include <sys/mman.h>
+#include <math.h>
+#include <unistd.h>
+#include <errno.h>
+#include <SDL/SDL.h>
+
+void decryptbuf(char * buf, int len) {
+	int i;
+	unsigned char pad = 5;
+	for(i = 0; i < len; i++) {
+		buf[i] ^= pad;
+		pad += 0xC1;
+	}
+}
+
+MNGFile::MNGFile(string n) {
+	name = n;
+	FILE * temp; // XXX: zap this
+	char filename[255];
+	
+	f = fopen(name.c_str(), "r");
+	if(!f) {
+		throw MNGFileException("Can't open file", errno);
+	}
+	
+	// Hack to obtain the filesize
+
+	fseek(f, 0, SEEK_END);
+	filesize = ftell(f);
+	
+	// mmap the data file
+	map = (char *) mmap(0, filesize, PROT_READ, MAP_PRIVATE, fileno(f), 0);
+	if(map == (void *) -1) {
+		throw MNGFileException("Can't mmap", errno);
+	}
+	
+	// Read metavariables from beginning of file
+
+	numsamples = swapEndianLong(*((int *) map));
+	scriptoffset = swapEndianLong(*(((int *) map) + 1));
+	scriptend = swapEndianLong(*(((int *) map) + 3));
+	scriptlength = scriptend - scriptoffset;
+
+	// Read and decode the MNG script
+
+	script = (char *) malloc(scriptlength * sizeof(char));
+	if(! script) throw MNGFileException("malloc failed", errno);
+	memcpy(script, map + scriptoffset, scriptlength);
+	decryptbuf(script, scriptlength);
+	
+	/* XXX: Parsing code
+	while((res = yylex()) != 0) {
+		printf("Token encountered: %s", names[res]);
+		if(res == T_TOKEN) printf(" token: %s", lex_token);
+		if(res == T_NUMBER) printf(" number: %f", lex_number);
+		if(res == T_NAME) printf(" name: %s", lex_text); 
+		if(res == T_WAVE) numwaves++;
+		printf("\n"); 
+	}  */
+
+	for(int i = 0; i < numsamples; i++) {
+		// Sample offsets and lengths are stored in pairs after the initial 16 bytes
+		int position = swapEndianLong(*((int *) map + 3 + (2 * i)));
+
+		// skip four bytes of the WAVE header, four of the FMT header, 
+		// the FMT chunk and four of the DATA header
+		position += swapEndianLong(*(int *)(map + position)) + 8;
+
+		int size = swapEndianLong(*((int *) (map + position)));
+		position += 4; // Skip the size field
+		
+		samples.push_back(make_pair(string("hi"), make_pair(map + position, size)));
+	}
+}
+
+void MNGFile::enumerateSamples() {
+	vector< pair< string, pair< char *, int > > >::iterator i;
+	for(i = samples.begin(); i != samples.end(); i++) {
+		printf("Position: %i Length: %i\n", (*i).second.first, (*i).second.second);
+		// PlaySound((*i).second.first, (*i).second.second);
+	}
+}
+
+
+MNGFile::~MNGFile() {
+	free(script);
+	munmap(map, filesize);
+	fclose(f);
+}	
+
---
+++ mngfile.h
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include "exceptions.h"
+
+using namespace std;
+
+class MNGFile {
+	private:
+		FILE * f;
+		long filesize;
+		char * map;
+		string name;
+		int numsamples, scriptoffset, scriptlength, scriptend;
+		char * script;
+		vector< pair< string, pair< char *, int > > > samples;
+	
+	public:
+		 MNGFile(string);
+		 void enumerateSamples();
+		 ~MNGFile();
+};
+	
+class MNGFileException : public creaturesException {
+	public:
+		int error;
+		MNGFileException(const char * m, int e) throw() : creaturesException(m) { error = e; }
+};
+
+class MNGNode {
+};
+
+class MNGTrack : public MNGNode {
+};
+
+class MNGLayer : public MNGNode {
+};
+
+class MNGLoopLayer : public MNGLayer {
+};
+
+class MNGAleatoricLayer : public MNGLayer {
+};
+
+class MNGVariable : public MNGNode {
+};
+
+class MNGUpdate : public MNGNode {
+};
+
