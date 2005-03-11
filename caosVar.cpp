697eb435f37bd52389f2faa55454c3236eb78769
Alyssa Milburn <fuzzie@fuzzie.org>
2005-03-11 20:23:29 +0000

    oops, caosvar.patch added caosVar.cpp and caosVar.h, so here they are

---
+++ caosVar.cpp
@@ -0,0 +1,85 @@
+/*
+ *  caosVar.cpp
+ *  openc2e
+ *
+ *  Created by Bryan Donlan on Thu Mar 10 2005.
+ *  Copyright (c) 2004 Alyssa Milburn and Bryan Donlan. All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ */
+
+#define IN_CAOSVAR_CPP
+
+#include "caosVar.h"
+
+#include <iostream>
+#include <sstream>
+
+#ifdef DONT_INLINE_CAOSVAR_ACCESSORS
+#error meh, copy the stuff out of caosVar.h and drop it here
+#endif
+
+static inline std::string stringify(double x) {
+        std::ostringstream o;
+        if (!(o << x)) throw "stringify() failed";
+        return o.str();
+}
+
+std::string caosVar::dump() const {
+	if (type == STRING) {
+		return std::string("\"") + string + "\" ";
+	} else if (type == INTEGER) {
+		return stringify(values.intValue) + " ";
+	} else if (type == FLOAT) {
+		return stringify(values.floatValue) + " ";
+	} else {
+		return "[bad caosVar!] ";
+	}
+}
+
+bool caosVar::operator == (const caosVar &v) const {
+	// todo: should be able to compare int and float, apparently
+	if (this->hasInt() && v.hasInt()) {
+		return this->getInt() == v.getInt();
+	} else if (this->hasDecimal() && v.hasDecimal()) {
+		return this->getFloat() == v.getFloat();
+	} else if (this->hasString() && v.hasString()) {
+		return this->string == v.string;
+	} else if (this->hasAgent() && v.hasAgent()) {
+		return this->getAgent() == v.getAgent();
+	}
+
+	std::cerr << "caosVar operator == couldn't compare " << this->dump() << " and " << v.dump() << "\n";
+	return false;
+}
+
+bool caosVar::operator > (const caosVar &v) const {
+	if (this->hasDecimal() && v.hasDecimal()) {
+		return this->getFloat() > v.getFloat();
+	}
+	// todo: should be able to compare strings, apparently
+	
+	std::cerr << "caosVar operator > couldn't compare " << this->dump() << " and " << v.dump() << "\n";
+	return false;
+}
+
+bool caosVar::operator < (const caosVar &v) const {
+	if (this->hasDecimal() && v.hasDecimal()) {
+		return this->getFloat() < v.getFloat();
+	}
+	// todo: should be able to compare strings, apparently
+	
+	std::cerr << "caosVar operator < couldn't compare " << this->dump() << " and " << v.dump() << "\n";
+	return false;
+}
+
+
---
+++ caosVar.h
@@ -0,0 +1,149 @@
+/*
+ *  caosVar.h
+ *  openc2e
+ *
+ *  Created by Bryan Donlan on Thu Mar 10 2005.
+ *  Copyright (c) 2004 Alyssa Milburn and Bryan Donlan. All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ */
+
+#ifndef CAOSVAR_H
+#define CAOSVAR_H 1
+
+#include "openc2e.h"
+#include <string>
+#include <cassert>
+
+class Agent;
+
+class caosVar {
+	protected:
+		enum variableType {
+			NULLTYPE = 0, AGENT, INTEGER, FLOAT, STRING, VARREF
+		};
+
+		variableType type;
+
+		union {
+			int intValue;
+			float floatValue;
+			Agent *agent;
+		} values;
+
+		std::string string;
+		// XXX: GET RID OF THIS
+		caosVar *vptr;
+
+	public:
+		void reset() {
+			string.clear();
+			type = NULLTYPE;
+			vptr = NULL;
+		}
+
+		caosVar() {
+			vptr = NULL;
+			type = INTEGER;
+			values.intValue = 0;
+		}
+
+		caosVar(const caosVar &copyFrom) {
+			string = copyFrom.string;
+			type = copyFrom.type;
+			values = copyFrom.values;
+			vptr = copyFrom.vptr;
+		}
+
+		bool isEmpty() const { return type == NULLTYPE; }
+		bool hasInt() const { return type == INTEGER; }
+		bool hasFloat() const { return type == FLOAT; }
+		bool hasAgent() const { return type == AGENT; }
+		bool hasString() const { return type == STRING; }
+		bool hasDecimal() const { return type == INTEGER || type == FLOAT; }
+		
+		void setInt(int i) { reset(); type = INTEGER; values.intValue = i; }
+		void setFloat(float i) { reset(); type = FLOAT; values.floatValue = i; }
+		void setAgent(Agent *i) { reset(); type = AGENT; values.agent = i; }
+		void setString(const std::string &i) {
+			type = STRING;
+			string = i;
+		}
+
+		int getInt() const {
+			caos_assert(hasDecimal());
+			if (type == INTEGER) {
+				return values.intValue;
+			} else {
+				return (int)values.floatValue;
+			}
+		}
+
+		float getFloat() const {
+			caos_assert(hasDecimal());
+			if (type == FLOAT) {
+				return values.floatValue;
+			} else {
+				return (float)values.intValue;
+			}
+		}
+
+		void getString(std::string &s) const {
+			caos_assert(hasString());
+			s = string;
+		}
+
+		std::string getString() const {
+			caos_assert(hasString());
+			return string;
+		}
+
+		Agent *getAgent() const {
+			caos_assert(hasAgent());
+			return values.agent;
+		}
+
+		// XXX: GET RID OF THIS
+		void setVariable(caosVar *v) {
+			vptr = v;
+		}
+		bool hasVariable() const {
+			return vptr;
+		}
+		caosVar *getVariable() const {
+			return vptr;
+		}
+
+		bool operator == (const caosVar &v) const;
+		bool operator != (const caosVar &v) const { return !(*this == v); }
+		bool operator > (const caosVar &v) const;
+		bool operator < (const caosVar &v) const;
+
+		std::string dump() const;
+};
+
+// Compatibility hacks
+// All of these are deprecated, of course
+
+#ifndef IN_CAOSVAR_CPP
+#define floatValue getFloat()
+#define intValue getInt()
+
+// Grep for this later and replace with reference-passing version
+#define stringValue getString()
+#define agentValue getAgent()
+
+// XXX: GET RID OF THIS
+#define variableValue getVariable()
+#endif
+
+#endif
