74367d13cc91508fbdb97725d4dcd75649f5fa08
Alyssa Milburn <fuzzie@fuzzie.org>
2005-03-11 22:33:12 +0000

    whoops, add AgentRef.cpp and AgentRef.h from bdonlan's kill.patch

---
+++ AgentRef.cpp
@@ -0,0 +1,62 @@
+/*
+ *  AgentRef.cpp
+ *  openc2e
+ *
+ *  Created by Bryan Donlan on The Apr 11 2005
+ *  Copyright (c) 2005 Bryan Donlan. All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ */
+
+#include "AgentRef.h"
+#include "Agent.h"
+#include <cassert>
+#include <iostream>
+
+void AgentRef::clear() {
+	if (!ref) return;
+	next->prev = prev;
+	prev->next = next;
+	ref = NULL;
+	next = prev = NULL;
+}
+
+void AgentRef::set(Agent *a) {
+	clear();
+	if (!a) return;
+
+	// Verify consistency of the linked list
+	assert(a->self.ref == a);
+	assert(a->self.next->prev = &a->self);
+	assert(a->self.prev->next = &a->self);
+
+	// We insert ourselves at a->self.next
+	next = a->self.next;
+	prev = &a->self;
+
+	next->prev = this;
+	prev->next = this;
+
+	ref = a;
+}
+
+void AgentRef::set(const AgentRef &r) {
+	/* Since r is const, we can't touch it directly.
+	 * Pass its ref to set(Agent*)
+	 */
+	set(r.ref);
+}
+
+void AgentRef::dump() const {
+	std::cerr << "AgentRef " << (void *)this << " pointing to " << (void *)ref << ", next=" << (void *)next <<
+		" prev=" << (void *)prev << std::endl;
+}
---
+++ AgentRef.h
@@ -0,0 +1,67 @@
+/*
+ *  AgentRef.h
+ *  openc2e
+ *
+ *  Created by Bryan Donlan on The Apr 11 2005
+ *  Copyright (c) 2005 Bryan Donlan. All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ */
+
+#ifndef AGENTREF_H
+#define AGENTREF_H 1
+
+#include <cstdlib> // for NULL
+#include <iostream>
+class Agent;
+
+class AgentRef {
+	friend class Agent;
+	
+protected:
+	AgentRef *next, *prev;
+	Agent *ref;
+
+public:
+	void dump() const;
+	AgentRef() { ref = NULL; next = NULL; prev = NULL; }
+	AgentRef(Agent *a) { ref = NULL; next = NULL; prev = NULL; set(a);
+		}
+	AgentRef(const AgentRef &r) {
+		ref = NULL; next = NULL; prev = NULL; set(r);
+	}
+
+	void clear();
+	
+	virtual ~AgentRef() { clear(); };
+
+	AgentRef &operator=(const AgentRef &r) { set(r); return *this; }
+	Agent *operator=(Agent *a) { set(a); return a; }
+	Agent &operator*() const { return *ref; }
+	Agent *operator->() const { return ref; }
+	bool operator!() const { return !ref; }
+	operator bool() const { return ref; }
+	operator Agent *() const { return ref; }
+	bool operator==(const AgentRef &r) const { return r.ref == ref; }
+	bool operator==(const Agent *r) const { return r == ref; }
+	bool operator!=(const AgentRef &r) const { return !(*this == r);}
+	bool operator!=(const Agent *r) const { return !(*this == r); }
+
+	void set(Agent *a);
+	void set(const AgentRef &r);
+
+	Agent *get() const { return ref; }
+};
+		
+
+#endif
+
