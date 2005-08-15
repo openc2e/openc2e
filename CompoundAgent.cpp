266951e120b543839f5e9e74aad0309b10a87b0a
Alyssa Milburn <fuzzie@fuzzie.org>
2005-08-15 02:46:18 +0000

    comment out core rendering

--- CompoundAgent.cpp
+++ CompoundAgent.cpp
@@ -29,12 +29,12 @@ void CompoundAgent::render(SDLBackend *renderer, int xoffset, int yoffset) {
 	}
 
 	// draw core
-	int xoff = xoffset + x;
+	/*int xoff = xoffset + x;
 	int yoff = yoffset + y;
 	renderer->renderLine(xoff + (getWidth() / 2), yoff, xoff + getWidth(), yoff + (getHeight() / 2), 0xFF0000CC);
 	renderer->renderLine(xoff + getWidth(), yoff + (getHeight() / 2), xoff + (getWidth() / 2), yoff + getHeight(), 0xFF0000CC);
 	renderer->renderLine(xoff + (getWidth() / 2), yoff + getHeight(), xoff, yoff + (getHeight() / 2), 0xFF0000CC);
-	renderer->renderLine(xoff, yoff + (getHeight() / 2), xoff + (getWidth() / 2), yoff, 0xFF0000CC);
+	renderer->renderLine(xoff, yoff + (getHeight() / 2), xoff + (getWidth() / 2), yoff, 0xFF0000CC); */
 }
 
 void CompoundAgent::addPart(CompoundPart *p) {
