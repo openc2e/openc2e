d7dd2c52fc3c76ba350a529d66c08a844297e222
Bryan Donlan <bdonlan@gmail.com>
2005-09-04 23:30:45 +0000

    r43694@bd:  bdonlan | 2005-09-04 19:29:59 -0400  Mark autogen.sh executable, change version detection logic

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
-AM_VERS=-1.9
-AC_VERS=2.50
+if [ -x /usr/bin/automake-1.9 ]
+then
+    AM_VERS=-1.9
+else
+    AM_VERS=
+fi
+if [ -x /usr/bin/autoheader2.50 ]
+then
+    AC_VERS=2.50
+else
+    AC_VERS=
+fi
 
 cat m4/* > acinclude.m4 &&
 libtoolize --force &&
