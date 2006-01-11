96661c8aab62f5f1c2b64b36454e41d772bad395
Bryan Donlan <bdonlan@gmail.com>
2006-01-11 03:14:59 +0000

    First implementation of an RTree. No erasure support yet.

---
+++ rtree.cpp
@@ -0,0 +1,113 @@
+#include "rtree.h"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <cstdio>
+
+struct rcmp
+{
+	bool operator()(const Region &r1, const Region &r2) 
+	{
+#define CMP(v) \
+		do { if (r1.v != r2.v) return r1.v < r2.v; } while (0)
+		CMP(xmin); CMP(xmax); CMP(ymin); CMP(ymax);
+#undef CMP
+	}
+};
+
+Region random_in(const Region &tr) {
+	double rp1 = (double)rand() / (double)RAND_MAX;
+	double rp2 = (double)rand() / (double)RAND_MAX;
+
+	int pt1 = tr.xmin + rp1 * (double)(tr.xmax - tr.xmin);
+	int pt2 = tr.ymin + rp1 * (double)(tr.ymax - tr.ymin);
+
+	return Region(pt1, pt1, pt2, pt2);
+
+}
+
+int main() {
+	try {
+		std::vector<Region> testVec;
+		srand(time(NULL));
+		RTree<Region> tree;
+		for (int i = 0; i < 5000; i++) {
+			int x1 = rand() % 50000;
+			int x2 = x1 + rand() % 125;
+			int y1 = rand() % 50000;
+			int y2 = y1 + rand() % 125;
+
+			if (x1 > x2) {
+				int t = x1;
+				x1 = x2;
+				x2 = t;
+			}
+			
+			if (y1 > y2) {
+				int t = y1;
+				y1 = y2;
+				y2 = t;
+			}
+			Region r(x1, x2, y1, y2);
+			
+			if (tree.find_one(r)) {
+				i--;
+				continue;
+			}
+				
+			testVec.push_back(Region(x1, x2, y1, y2));
+		}
+
+		bool allok = true;
+		
+		int ct = 0;
+		for (std::vector<Region>::iterator i = testVec.begin(); i != testVec.end(); i++) {
+			tree.insert(*i, *i);
+			ct++;
+//			if (ct % 100 == 0)
+//				fprintf(stderr, "\rins %06d", ct);
+		}
+		std::cerr << std::endl << "size=" << tree.size() << "inner=" << tree.inner_size() << std::endl;
+
+		ct = 0;
+		for (std::vector<Region>::iterator i = testVec.begin(); i != testVec.end(); i++) {
+			if (ct++ % 100 == 0)
+				fprintf(stderr, "\rchk %06d", ct);
+			std::cout << "Verify of " << (*i).to_s() << "... ";
+			Region q = random_in(*i);
+			assert(q.overlaps((*i)));
+			std::cout << q.to_s() << "... ";
+			Region *result = tree.find_one(q);
+			bool ok = false;
+			if (result && *result == *i) {
+				ok = true;
+			} else {
+				std::vector<RTree<Region>::ptr> results = tree.find(q);
+				std::cout << results.size() << " results ... ";
+				for (std::vector<RTree<Region>::ptr>::iterator ci = results.begin(); ci != results.end(); ci++) {
+					if ((*ci).data() == *i) {
+						ok = true;
+						break;
+					}
+				}
+			}
+// */
+			if (ok)
+				std::cout << "ok";
+			else
+				std::cout << "nok";
+			std::cout << std::endl;
+			allok = allok && ok;
+		}	
+
+		if (!allok)
+			std::cout << "Not all ok";
+		else
+			std::cout << "All ok";
+		std::cout << std::endl;
+	} catch (std::exception &e) {
+		std::cout << "abort: " << e.what();
+	}
+	std::cout << std::endl;
+	fprintf(stderr, "\n");
+}
---
+++ rtree.h
@@ -0,0 +1,513 @@
+#ifndef RTREE_H
+#define RTREE_H 1
+
+#include <vector>
+#include <string>
+#include <climits>
+#include <sstream>
+#include <cassert>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <list>
+
+#define REGION_MAX 50
+#define REGION_MIN 24
+
+typedef unsigned long long u64_t;
+
+struct Region {
+	int xmin, xmax;
+	int ymin, ymax;
+
+	bool contains(int x, int y) const {
+		return (x >= xmin && x <= xmax &&
+				y >= ymin && y <= ymax
+			   );
+	}
+
+	bool overlaps(const Region &r2) const {
+		return (xmin <= r2.xmax && xmax >= r2.xmin)
+			&& (ymin <= r2.ymax && ymax >= r2.ymin);
+	}
+	
+	Region expand(const Region &r2) {
+		Region r1 = *this;
+		if (r1.xmin > r2.xmin)
+			r1.xmin = r2.xmin;
+		if (r1.xmax < r2.xmax)
+			r1.xmax = r2.xmax;
+		if (r1.ymin > r2.ymin)
+			r1.ymin = r2.ymin;
+		if (r1.ymax < r2.ymax)
+			r1.ymax = r2.ymax;
+		return r1;
+	}
+
+	u64_t area() const {
+		return (u64_t)(xmax - xmin) * (u64_t)(ymax - ymin);
+	}
+
+	std::string to_s() const {
+		std::ostringstream oss;
+		oss << "(" << xmin << " - " << xmax << ", " << ymin << " - " << ymax << ")";
+		return oss.str();
+	}
+
+	bool operator==(const Region &r2) const {
+		return (xmin == r2.xmin)
+			&& (xmax == r2.xmax)
+			&& (ymin == r2.ymin)
+			&& (ymax == r2.ymax)
+			;
+	}
+
+	bool operator!=(const Region &r2) const {
+		return !(*this == r2);
+	}
+
+	Region(const Region &r2)
+		: xmin(r2.xmin), xmax(r2.xmax),
+		  ymin(r2.ymin), ymax(r2.ymax)
+	{
+		assert(xmin <= xmax);
+		assert(ymin <= ymax);
+	}
+
+	Region(int xmin_, int xmax_, int ymin_, int ymax_)
+		: xmin(xmin_), xmax(xmax_), ymin(ymin_), ymax(ymax_)
+	{
+		assert(xmin <= xmax);
+		assert(ymin <= ymax);
+	}
+	
+};
+
+template <class T> struct RBranch;
+template <class T> struct RData;
+
+template <class T>
+struct RNode {
+	Region r;
+	RBranch<T> *parent;
+	int parent_idx;
+
+	virtual void scan(const Region &target, std::vector<RData<T> *> &list) = 0;
+	virtual RData<T> *scan(const Region &target) = 0;
+
+	virtual RBranch<T> *chooseleaf(const Region &)
+	{ assert(false); }
+
+	RNode(const Region &r_) : r(r_), parent(NULL), parent_idx(-1)
+	{
+	}
+
+	virtual ~RNode() { }
+
+	virtual std::string dump() = 0;
+
+	virtual size_t size() const = 0; // Leaves
+	virtual size_t inner_size() const = 0; // All nodes
+	virtual void expensive_checks() {}
+};
+
+template <class T>
+struct RBranch : public RNode<T> {
+	int child_count; // : CLD_BITS;
+	bool is_leaf : 1; // if true, all of children are RLeafs 
+	RNode<T> *children[REGION_MAX];
+
+	size_t sz, isz;
+
+	virtual size_t size() const { return sz; }
+
+	virtual size_t inner_size() const { return isz; }
+
+	void expensive_checks(bool local = false) {
+		Region r_ = this->r;
+		for (int i = 0; i < child_count; i++) {
+			assert(children[i]->parent == this);
+			assert(children[i]->parent_idx == i);
+			children[i]->expensive_checks();
+		}
+		minimize();
+		if (this->r != r_) {
+			std::cerr << "expensive_checks() fail, was = " << r_.to_s() << std::endl;
+			std::cerr << dump();
+			abort();
+		}
+	}
+
+	void minimize() {
+		assert(child_count);
+		this->sz = children[0]->size();
+		this->isz = children[0]->inner_size() + 1;
+		this->r = children[0]->r;
+		for (int i = 1; i < child_count; i++) {
+			this->r = this->r.expand(children[i]->r);
+			this->sz += children[i]->size();
+			this->isz += children[i]->inner_size();
+		}
+	}
+
+	void recalc_up() {
+		RBranch<T> *p = this;
+		while(p) {
+			p->minimize();
+			p = p->parent;
+		}
+	}
+	
+	void add_kid(RNode<T> *n) {
+		assert(child_count < REGION_MAX);
+		children[child_count] = n;
+		n->parent = this;
+		n->parent_idx = child_count;
+		child_count++;
+
+		recalc_up();
+	}
+
+	void drop_kid(int idx) { // not yet used
+		assert(child_count && idx >= 0 && idx < child_count);
+		if (idx == child_count - 1)
+			child_count--;
+		for (int i = idx; i < child_count - 1; i++) {
+			children[i] = children[i + 1];
+			children[i]->parent_idx = i;
+		}
+	}
+
+	void add_multi(RNode<T> **n, int count) {
+		assert(child_count + count <= REGION_MAX);
+		for (int i = 0; i < count; i++) {
+			children[child_count + i] = n[i];
+			children[child_count + i]->parent = this;
+			children[child_count + i]->parent_idx = child_count + i;
+		}
+
+		child_count += count;
+
+		recalc_up();
+	}
+
+	RBranch(const Region &r) : RNode<T>(r) {
+		// Convenience for the main RTree initializer
+		child_count = 0;
+		is_leaf = true;
+		this->sz = 0;
+		this->isz = 1;
+	}
+
+	virtual void scan(const Region &target, std::vector<RData<T> *> &list) {
+		if (!this->r.overlaps(target))
+			return;
+		for (int i = 0; i < child_count; i++)
+			if (children[i]->r.overlaps(target))
+				children[i]->scan(target, list);
+	}
+
+	virtual RData<T> *scan(const Region &target) {
+		if (!this->r.overlaps(target))
+			return NULL;
+		for (int i = 0; i < child_count; i++) {
+			if (children[i]->r.overlaps(target)) {
+				RData<T> *ret = children[i]->scan(target);
+				if (ret) return ret;
+			}
+		}
+		return NULL;
+	}
+
+	virtual RBranch<T> *chooseleaf(const Region &target) {
+		if (is_leaf)
+			return this;
+		assert(child_count);
+		RBranch<T> *best = dynamic_cast<RBranch<T> *>(children[0]);
+		unsigned long expansion = ULONG_MAX;
+
+		for (int i = 0; i < child_count; i++) {
+			u64_t orig_area = children[i]->r.area();
+			u64_t new_area  = children[i]->r.expand(target).area();
+			u64_t exp       = new_area - orig_area;
+			if (exp < expansion) {
+				best = dynamic_cast<RBranch<T> *>(children[i]);
+				assert(best);
+				expansion = exp;
+			}
+		}
+		return best->chooseleaf(target);
+	}
+
+	void insert(RNode<T> *node, RBranch<T> *&root) {
+		if (is_leaf)
+			assert(!dynamic_cast<RBranch<T> *>(node));
+		else
+			assert(dynamic_cast<RBranch<T> *>(node));
+		
+		if (child_count == REGION_MAX)
+			split(root);
+		add_kid(node);
+		expensive_checks();
+	}
+
+	void divide(std::list<RNode<T> *> &inlist, std::list<RNode<T> *> &node1, std::list<RNode<T> *> &node2) {
+		// quadratic time split
+		// Described in Guttman, A. 1988. R-trees: a dynamic index structure for spatial searching.
+		// section 3.5.2
+		
+		// PickSeeds
+		size_t orig_size = inlist.size();
+
+		assert(!node1.size() && !node2.size());
+		
+		typename std::list<RNode<T> *>::iterator best1, best2;
+		u64_t best_d = 0;
+		typename std::list<RNode<T> *>::iterator i1, i2;
+
+		for (i1 = inlist.begin(); i1 != inlist.end(); i1++) {
+			for (i2 = inlist.begin(); i2 != inlist.end(); i2++) {
+				if (i1 == i2)
+					continue;
+				Region j = (*i1)->r.expand((*i2)->r);
+				u64_t d = j.area() - (*i1)->r.area() - (*i2)->r.area();
+				if (d >= best_d) {
+					best_d = d;
+					best1 = i1;
+					best2 = i2;
+				}
+			}
+		}
+
+		Region r1((*best1)->r);
+		Region r2((*best2)->r);
+		
+		node1.push_back(*best1);
+		node2.push_back(*best2);
+		inlist.erase(best1);
+		inlist.erase(best2);
+
+
+		while (inlist.size()) {
+			if (inlist.size() + node1.size() == REGION_MIN) {
+				std::copy(inlist.begin(), inlist.end(), std::inserter(node1, node1.end()));
+				inlist.clear();
+				return;
+			}
+			if (inlist.size() + node2.size() == REGION_MIN) {
+				std::copy(inlist.begin(), inlist.end(), std::inserter(node2, node2.end()));
+				inlist.clear();
+				return;
+			}
+			// PickNext
+			typename std::list<RNode<T> *>::iterator best;
+			u64_t best_diff = 0;
+
+			for (typename std::list<RNode<T> *>::iterator i = inlist.begin(); i != inlist.end(); i++) {
+				Region &r = (*i)->r;
+				u64_t d1 = r1.expand(r).area() - r1.area();
+				u64_t d2 = r2.expand(r).area() - r2.area();
+				u64_t diff = (d1 < d2) ? d2 - d1 : d1 - d2;
+				if (diff >= best_diff) {
+					best = i;
+					best_diff = diff;
+				}
+			}
+
+			Region &r = (*best)->r;
+			u64_t d1 = r1.expand(r).area() - r1.area();
+			u64_t d2 = r2.expand(r).area() - r2.area();
+			int choice = 0; // node1, node2
+
+			if (d1 > d2)
+				choice = 2;
+			else if (d2 > d2)
+				choice = 1;
+			else if (r1.area() > r2.area())
+				choice = 2;
+			else if (r2.area() > r1.area())
+				choice = 1;
+			else if (node1.size() > node2.size())
+				choice = 2;
+			else if (node2.size() > node1.size())
+				choice = 2;
+			else choice = 1; // arbitrary tiebreaker
+			
+			assert(choice);
+			if (choice == 1) {
+				r1 = r1.expand(r);
+				node1.push_back(*best);
+			} else {
+				r2 = r2.expand(r);
+				node2.push_back(*best);
+			}
+
+			inlist.erase(best);
+		}
+
+		assert(orig_size == node1.size() + node2.size());
+	}
+
+	void split(RBranch<T> *&root) {
+		assert(child_count == REGION_MAX);
+		
+		int split = child_count / 2;
+		RBranch<T> *newbranch = new RBranch<T>(children[split]->r);
+
+		newbranch->is_leaf = this->is_leaf;
+
+		std::list<RNode<T> *> l, n1, n2;
+		std::copy(children, children + child_count, std::inserter(l, l.end()));
+		assert(l.size() == child_count);
+		divide(l, n1, n2);
+		assert(l.size() == 0);
+		assert(n1.size() + n2.size() == REGION_MAX);
+
+		std::copy(n1.begin(), n1.end(), children);
+		std::copy(n2.begin(), n2.end(), newbranch->children);
+		
+		child_count = n1.size();
+		newbranch->child_count = n2.size();
+
+		for (int i = 0; i < child_count; i++) {
+			children[i]->parent = this;
+			children[i]->parent_idx = i;
+		}
+		
+		for (int i = 0; i < newbranch->child_count; i++) {
+			newbranch->children[i]->parent = newbranch;
+			newbranch->children[i]->parent_idx = i;
+		}
+		
+		minimize();
+		newbranch->minimize();
+		
+		if (!this->parent) { // Need to make a new root!
+			root = new RBranch<T>(this->r);
+			root->is_leaf = false;
+			root->insert(this, root);
+		}
+
+		this->parent->insert(newbranch, root);
+		
+//		std::cerr << "Split! After split, myct " << this->child_count <<
+//			" newbranch " << newbranch->child_count << " root " << root->child_count << std::endl;
+
+		expensive_checks();
+		root->expensive_checks();
+	}
+
+	virtual std::string dump() {
+		assert(child_count);
+		Region test(children[0]->r);
+		std::ostringstream oss;
+		oss << "RBranch " << this->r.to_s() << " {" << std::endl;
+		for (int i = 0; i < child_count; i++) {
+			oss << "#" << i << " " << children[i]->dump() << std::endl;
+			test = test.expand(children[i]->r);
+		}
+		if (test == this->r)
+			oss << "OK ";
+		else {
+			Region test2(test);
+			oss << "NOK " << test.to_s();
+			test2.expand(this->r);
+			if (test2 != test) oss << " NOTMIN ";
+		}
+		oss << "}" << std::endl;
+		return oss.str();
+	}
+};
+
+template<class T>
+struct RData : public RNode<T> {
+	T obj;
+
+	virtual size_t size() const { return 1; }
+
+	virtual size_t inner_size() const { return 1; }
+	
+	virtual void scan(const Region &target, std::vector<RData<T> *> &list) {
+		if (!this->r.overlaps(target))
+			return;
+		list.push_back(this);
+	}
+	virtual RData<T> *scan(const Region &target) {
+		if (this->r.overlaps(target))
+			return this;
+		return NULL;
+	}
+
+	RData(const Region &r_, const T &obj_) : RNode<T>(r_), obj(obj_) { }
+
+	std::string dump() {
+		std::ostringstream oss;
+		oss << "RData " << this->r.to_s() << " -> " << /* obj.to_s() */ (void *)&obj << std::endl;
+		return oss.str();
+	}
+};
+
+template <class T>
+class RTree {
+	protected:
+		RBranch<T> *root;
+	public:
+
+		class ptr {
+			// Wrapper to enforce encapsulation
+			protected:
+				RData<T> *node;
+			public:
+				const Region &region() const { return node->r; }
+				T &data() { return node->obj; }
+			protected:
+				friend std::vector<ptr> RTree::find(const Region &r);
+				ptr(RData<T> *n) : node(n) { }
+				ptr(RData<T> &n) : node(&n) {}
+			public:
+				ptr(const ptr &p) : node(p.node) {}
+		};
+		
+		RTree() : root(NULL) {}
+		
+		void insert(const Region &r, const T &val) {
+			RData<T> *node = new RData<T>(r, val);
+			if (!root)
+				root = new RBranch<T>(r);
+			root->chooseleaf(r)->insert(node, root);
+			check();
+		}
+		std::vector<ptr> find(const Region &r) {
+			std::vector<RData<T> *> list;
+			root->scan(r, list);
+
+			typename std::vector<RData<T> *>::iterator i = list.begin();
+			std::vector<ptr> out;
+			while (i != list.end()) {
+				out.push_back(ptr(*i));
+				i++;
+			}
+
+			return out;
+		}
+
+		T *find_one(const Region &r) {
+			if (!root) return NULL;
+			
+			RData<T> *ret = root->scan(r);
+			if (!ret)  return NULL;
+			return &ret->obj;
+		}
+
+		std::string dump() {
+			if (!root)
+				return "NULL";
+			return root->dump();
+		}
+
+		void check() { if (root) root->expensive_checks(); }
+		size_t size() const { return root ? root->size() : 0; }
+		size_t inner_size() const { return root ? root->inner_size() : 0; }
+};
+
+#endif
