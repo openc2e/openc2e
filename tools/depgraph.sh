53d0a4f8ba6fb9d47d873866b15452c7846e163e
Bryan Donlan <bdonlan@gmail.com>
2006-04-07 05:30:59 +0000

    Fix depgraph.sh for headers in subdirs

--- tools/depgraph.sh
+++ tools/depgraph.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "digraph {"
 for i in "$@"; do echo "\"$i\";"; done
-grep '^[ \t]*#include "' "$@"|sed 's/^\([^:]*\):[ \t]*#include "\([^"]*\)".*$/"\2" -> "\1";/'|sort|uniq
+grep '^[ \t]*#include [<"]' "$@"|perl -ne 'm{^([^:]*):[ \t]*#include [<"]([^>"]*)[>"].*$}; -e $2 && print qq{"$2" -> "$1"};'|sort|uniq
 
 echo }
