93a62ba82c435f90c81691dcab779f9029328c00
Bryan Donlan <bdonlan@gmail.com>
2006-05-21 04:51:49 +0000

    add forgotten ser/s_genome.h, which actually has the boost glue ;)

---
+++ ser/s_genome.h
@@ -0,0 +1,26 @@
+#ifndef S_GENOME_H
+#define S_GENOME_H 1
+
+#include "genome.h"
+#include "serialization.h"
+#include <sstream>
+
+SAVE(genomeFile) {
+    std::ostringstream genes, notes;
+    genes << obj;
+    obj.writeNotes(notes);
+    std::string genestr = genes.str();
+    std::string notestr = notes.str();
+    ar & genestr & notestr;
+}
+
+LOAD(genomeFile) {
+    std::string genes, notes;
+    ar & genes & notes;
+    std::istringstream genestream(genes), notestream(notes);
+    genestream >> std::noskipws >> obj;
+//    obj.readNotes(notestream);
+}
+
+#endif
+
