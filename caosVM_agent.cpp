openc2e/openc2e

commit 4826b16e08cc4bffda92921e9ff0eae38ae93d4e

add engine version check for 8->7 message number conversion (don't look now, but i think creatures is made out of hacks)
git-svn-id: svn://openc2e.ccdevnet.org/openc2e/trunk@1289 5a3b5b51-85f2-0310-b56e-886e3cf1dda3

caosVM_agent.cpp
@@ -744,7 +744,10 @@ unsigned int calculateScriptId(unsigned int message_id) {
 		case 2: /* deactivate */
 			return 0;
 		case 8: /* hilarious special case to fix c2 airlock */
-			return 7;
+			if (engine.version < 3)
+				return 7;
+			else
+				return 8;
 		case 0: /* activate 1 */
 		case 1: /* activate 2 */
 			return message_id + 1;

commit 0ac51957a4a2bc9408e1ce885afe37f28b8d6400

build error introduced in r1289 fixed, doh

caosVM_agent.cpp
@@ -737,6 +737,7 @@ void caosVM::v_PNTR() {
 	result.setAgent(world.hand());
 }
 
+#include "Engine.h" // for engine.version check, sigh
 unsigned int calculateScriptId(unsigned int message_id) {
 	// aka, why oh why is this needed? Silly CL.
 