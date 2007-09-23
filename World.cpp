4b754e7433a0ea8e4635b842a4d8e6eaa65f8ea6
Alyssa Milburn <fuzzie@fuzzie.org>
2007-09-23 17:57:10 +0000

    the 'Startup' directory is part of the cvillage switcher too, so skip over it in non-switcher mode

--- World.cpp
+++ World.cpp
@@ -447,7 +447,8 @@ void World::executeBootstrap(bool switcher) {
 			for (fs::directory_iterator d(b); d != fsend; ++d) {
 				if (fs::exists(*d) && fs::is_directory(*d)) {
 					std::string s = (*d).leaf();
-					if (s == "000 Switcher") {
+					// TODO: cvillage has switcher code in 'Startup', so i included it here too
+					if (s == "000 Switcher" || s == "Startup") {
 						if (!switcher) continue;
 					} else {
 						if (switcher) continue;
