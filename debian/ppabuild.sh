c2de562d169f444e25b602f45867e4e35a9cb5e9
Bryan Donlan <bdonlan@gmail.com>
2008-02-08 22:16:03 +0000

    Use the same version for all suites in the PPA upload

--- debian/ppabuild.sh
+++ debian/ppabuild.sh
@@ -3,8 +3,8 @@
 
 trunk='svn://ccdevnet.org/openc2e/trunk'
 ppasuffix="ppa0"
-## These should be in order from earliest ubuntu release to latest
-set -A dists hardy gutsy
+#set -A suites hardy gutsy
+set -A suites gutsy
 
 rev=`svn info $trunk|grep '^Revision:'|sed 's/.*: //'`
 tempdir=`mktemp -d`
@@ -16,17 +16,18 @@ checkout=openc2e-0.0svn$rev
 svn export $trunk $checkout -r $rev
 tar czvf openc2e_0.0svn$rev.orig.tar.gz $checkout
 cp $checkout/debian/changelog $tempdir/changelog.orig
-distindex=0
-for dist in $dists; do
-	distindex=$(($distindex + 1))
-	pushd $checkout/debian
-	cp -f $tempdir/changelog.orig changelog
-	debversion="0.0svn${rev}-1~${ppasuffix}-${distindex}${dist}"
-	debchange -v $debversion -D $dist -b "Automatic checkout of revision ${rev}"
-	cd ..
-	dpkg-buildpackage -sa -rfakeroot -S
-	popd
-	dput -c $tempdir/$checkout/debian/dput.cf ccdevnet-ppa openc2e_${debversion}_source.changes
+
+pushd $checkout/debian
+cp -f $tempdir/changelog.orig changelog
+debversion="0.0svn${rev}-1~${ppasuffix}"
+debchange -v $debversion -D UNRELEASED -b "Automatic checkout of revision ${rev}"
+cd ..
+dpkg-buildpackage -sa -rfakeroot -S
+popd
+
+for suite in $suites; do
+	sed "s/SUITE/$suite/" < $tempdir/$checkout/debian/dput.cf > $tempdir/dput.cf
+	dput -c $tempdir/dput.cf ccdevnet-ppa openc2e_${debversion}_source.changes
 done
 
 rm -rf $tempdir
