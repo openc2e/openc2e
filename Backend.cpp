3b93d8792b859190aba52cabd7b9b1c38675d58c
Bryan Donlan <bdonlan@gmail.com>
2008-02-13 08:39:50 +0000

    Add missing Backend.cpp

---
+++ Backend.cpp
@@ -0,0 +1,17 @@
+#include "Engine.h"
+#include "World.h"
+#include "Backend.h"
+
+int Backend::main(int argc, char **argv) {
+	resize(800, 600);
+	
+	// do a first-pass draw of the world. TODO: correct?
+	world.drawWorld();
+
+	while (!engine.done) {
+		if (!engine.tick()) // if the engine didn't need an update..
+			delay(10); // .. delay for a short while
+	} // main loop
+
+	return 0;
+}
