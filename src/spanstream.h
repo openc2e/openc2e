5ad1e83d9a6bbcdf8fdcea1e93dd071dd837d387
Michael Maltese <michaeljosephmaltese@gmail.com>
2020-04-27 14:48:53 -0700

    spanstream: allow seeking, and fix when reading '\0' bytes

--- src/spanstream.h
+++ src/spanstream.h
@@ -5,26 +5,47 @@
 class spanstream : public std::istream
 {
 public:
-	spanstream(char* buffer, size_t buffer_size)
+	spanstream(unsigned char* buffer, size_t buffer_size)
 		: std::istream(&buf), buf(buffer, buffer_size) {}
 
-	spanstream(unsigned char* buffer, size_t buffer_size)
-		: spanstream(reinterpret_cast<char*>(buffer), buffer_size) {}
+	spanstream(char* buffer, size_t buffer_size)
+		: spanstream(reinterpret_cast<unsigned char*>(buffer), buffer_size) {}
 
 private:
 	class spanstreambuf : public std::streambuf
 	{
 	public:
-		char *buffer;
-		size_t buffer_size;
-
-		spanstreambuf(char* buffer_, size_t buffer_size_)
+		spanstreambuf(unsigned char* buffer_, size_t buffer_size_)
 			: buffer(buffer_), buffer_size(buffer_size_) {}
 
-		int_type uflow() {
-			return (position < buffer_size) ? buffer[position++] : EOF;
+	protected:
+		pos_type seekpos(pos_type pos, std::ios_base::openmode which) override {
+			return seekoff(pos, std::ios_base::beg, which);
+		}
+
+		pos_type seekoff(off_type off, std::ios_base::seekdir dir, std::ios_base::openmode which) override {
+			if (which == std::ios_base::out) {
+				return pos_type(off_type(-1)); // can't change output position
+			}
+			switch (dir) {
+				case std::ios_base::cur:
+					position = position + off;
+					return position;
+				case std::ios_base::end:
+					position = buffer_size + off;
+					return position;
+				case std::ios_base::beg:
+					position = off;
+					return position;
+			}
+		}
+
+		int_type uflow() override {
+			return (position < buffer_size) ? buffer[position++] : traits_type::eof();
 		}
 	private:
+		unsigned char *buffer;
+		size_t buffer_size;
 		size_t position = 0;
 	};
 	spanstreambuf buf;
