35c0a2cc09b460fefe4945e89cf8512cb5ed35f4
Michael Maltese <michaeljosephmaltese@gmail.com>
2020-05-02 10:47:25 -0700

    CMake: fixes around testing on Windows

--- cmake/Openc2eOutputOnTestFailure.cmake
+++ cmake/Openc2eOutputOnTestFailure.cmake
@@ -1,6 +1,6 @@
 if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.17.0")
 	list(APPEND CMAKE_CTEST_ARGUMENTS "--output-on-failure")
-else()
+elseif(NOT WINDOWS)
 	message("")
 	message(WARNING "CTest will hide test output even on failure. To fix this, either:
  1) Upgrade to CMake 3.17; or
