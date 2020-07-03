3a579100fd2af3b97a23d450dfaea10d36c75816
Michael Maltese <michaeljosephmaltese@gmail.com>
2020-07-02 17:58:40 -0700

    add stringutil.h

---
+++ src/stringutil.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+bool string_in(const std::string& value, std::initializer_list<std::string> strings);
+
+std::string lowerstring(std::string s);
+
+std::string upperstring(std::string s);
