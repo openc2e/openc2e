3311b3e428799a7e1dab15e909469551d4bbc364
Michael Maltese <michaeljosephmaltese@gmail.com>
2020-10-29 12:25:24 -0700

    Add optional.h

---
+++ src/optional.h
@@ -0,0 +1,52 @@
+#pragma once
+
+// Once openc2e goes to C++17, replace this with std::optional
+
+#include <exception>
+#include <type_traits>
+
+class bad_optional_access : public std::exception {
+};
+
+template <class T>
+class optional {
+public:
+  optional() = default;
+  optional(T value) {
+    has_value_ = true;
+    new(&storage_) T(value);
+  }
+  ~optional() {
+    if (has_value_) {
+      // needs std::launder in C++17
+      reinterpret_cast<T*>(&storage_)->~T();
+    }
+  }
+  explicit operator bool() const {
+    return has_value_;
+  }
+  bool has_value() const {
+    return has_value_;
+  }
+  T& operator*() {
+    // needs std::launder in C++17
+    return *reinterpret_cast<T*>(&storage_);
+  }
+  T& value() {
+    if (has_value_) {
+      // needs std::launder in C++17
+      return *reinterpret_cast<T*>(&storage_);
+    } else {
+      throw bad_optional_access();
+    }
+  }
+  
+private:
+  bool has_value_ = false;
+  typename std::aligned_storage<sizeof(T), alignof(T)>::type storage_;
+};
+
+template <class T>
+optional<T> make_optional(T value) {
+  return optional<T>(value);
+}
