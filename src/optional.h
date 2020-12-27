360244caf15aa338c79c705ff12f09feb9c4e677
Michael Maltese <michaeljosephmaltese@gmail.com>
2020-12-27 10:16:17 -0800

    optional: fix crash by defining copy, move, and assignment

--- src/optional.h
+++ src/optional.h
@@ -17,10 +17,33 @@ public:
     has_value_ = true;
     new(&storage_) T(value);
   }
+  optional(const optional& other) {
+      *this = other;
+  }
+  optional(optional&& other) {
+      *this = std::move(other);
+  }
+  optional& operator=(const optional& other) {
+      this->~optional();
+      has_value_ = other.has_value_;
+      if (has_value_) {
+          new(&storage_) T(*other);
+      }
+      return *this;
+  }
+  optional& operator=(optional&& other) {
+      this->~optional();
+      has_value_ = other.has_value_;
+      if (has_value_) {
+          storage_ = other.storage_;
+          other.has_value_ = false;
+      }
+      return *this;
+  }
   ~optional() {
     if (has_value_) {
-      // needs std::launder in C++17
-      reinterpret_cast<T*>(&storage_)->~T();
+      (&**this)->~T();
+      has_value_ = false;
     }
   }
   explicit operator bool() const {
@@ -33,16 +56,19 @@ public:
     // needs std::launder in C++17
     return *reinterpret_cast<T*>(&storage_);
   }
+  const T& operator*() const {
+    // needs std::launder in C++17
+    return *reinterpret_cast<const T*>(&storage_);
+  }
   T& value() {
     if (has_value_) {
-      // needs std::launder in C++17
-      return *reinterpret_cast<T*>(&storage_);
+      return **this;
     } else {
       throw bad_optional_access();
     }
   }
   T value_or(const T& default_value) {
-      if (*this) {
+      if (has_value_) {
           return **this;
       } else {
           return default_value;
