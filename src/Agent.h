openc2e/openc2e

commit b4810122539230aaca48c64206acd237dd88f817

Implement most direct display commands

src/Agent.h
@@ -218,6 +218,14 @@ class Agent : public std::enable_shared_from_this<Agent> {
 	int thrt = 0;
 	int size = 0;
 
+	// drawing
+	uint8_t alpha = 255;
+	bool draw_mirrored = false;
+	float scle = 1.0; // Sea-Monkeys only
+	bool strc = false; // Sea-Monkeys only
+	unsigned int strc_width; // Sea-Monkeys only
+	unsigned int strc_height; // Sea-Monkeys only
+
 	Agent(unsigned char f, unsigned char g, unsigned short s, unsigned int p);
 	virtual ~Agent();
 

src/CompoundPart.h
@@ -42,9 +42,6 @@ class CompoundPart : public renderable {
 	unsigned int zorder, id;
 	unsigned int part_sequence_number;
 
-	bool has_alpha;
-	unsigned char alpha;
-
 	virtual void render(class RenderTarget* renderer, int xoffset, int yoffset);
 	virtual void partRender(class RenderTarget* renderer, int xoffset, int yoffset) = 0;
 	virtual void tick() {}

src/SpritePart.cpp
@@ -22,7 +22,6 @@ SpritePart::SpritePart(Agent* p, unsigned int _id, std::string spritefile, unsig
 	is_transparent = (engine.version > 2);
 	framerate = 1;
 	framedelay = 0;
-	draw_mirrored = false;
 
 	if (sprite->numframes() <= firstimg) {
 		if (engine.gametype == "cv") {
@@ -54,7 +53,16 @@ void SpritePart::partRender(RenderTarget* renderer, int xoffset, int yoffset) {
 		}
 	}
 	assert(getCurrentSprite() < getSprite()->numframes());
-	renderer->renderCreaturesImage(getSprite(), getCurrentSprite(), xoffset + x, yoffset + y, has_alpha ? alpha : 0, draw_mirrored);
+	RenderOptions render_opts;
+	render_opts.alpha = parent->alpha;
+	render_opts.mirror = parent->draw_mirrored;
+	render_opts.scale = parent->scle;
+	if (parent->strc) {
+		render_opts.override_drawsize = true;
+		render_opts.overridden_drawwidth = parent->strc_width;
+		render_opts.overridden_drawheight = parent->strc_height;
+	}
+	renderer->renderCreaturesImage(getSprite(), getCurrentSprite(), xoffset + x, yoffset + y, render_opts);
 }
 
 void SpritePart::setFrameNo(unsigned int f) {

src/TextEntryPart.cpp
@@ -98,7 +98,9 @@ void TextEntryPart::partRender(RenderTarget* renderer, int xoffset, int yoffset)
 
 void TextEntryPart::renderCaret(RenderTarget* renderer, int xoffset, int yoffset) {
 	// TODO: fudge xoffset/yoffset as required
-	renderer->renderCreaturesImage(caretsprite, caretpose, xoffset, yoffset, has_alpha ? alpha : 0);
+	RenderOptions render_opts;
+	render_opts.alpha = parent->alpha;
+	renderer->renderCreaturesImage(caretsprite, caretpose, xoffset, yoffset, render_opts);
 }
 
 void TextEntryPart::tick() {

src/TextPart.cpp
@@ -1,5 +1,6 @@
 #include "TextPart.h"
 
+#include "Agent.h"
 #include "Backend.h"
 #include "Engine.h"
 #include "TextEntryPart.h"
@@ -262,7 +263,9 @@ void TextPart::partRender(RenderTarget* renderer, int xoffset, int yoffset, Text
 			if (((unsigned char)lines[i].text[x]) < 32)
 				continue; // TODO: replace with space or similar?
 			int spriteid = ((unsigned char)lines[i].text[x]) - 32;
-			renderer->renderCreaturesImage(sprite_to_use, spriteid, somex + currentx, yoff + currenty, has_alpha ? alpha : 0);
+			RenderOptions render_opts;
+			render_opts.alpha = parent->alpha;
+			renderer->renderCreaturesImage(sprite_to_use, spriteid, somex + currentx, yoff + currenty, render_opts);
 			if ((caretdata) && (caretdata->caretpos == lines[i].offset + x))
 				caretdata->renderCaret(renderer, somex + currentx, yoff + currenty);
 			currentx += textsprite->width(spriteid) + charspacing;

src/caos/caosVM_agent.cpp
@@ -1465,22 +1465,6 @@ void c_DCOR(caosVM* vm) {
 }
 
 /**
- MIRA (integer)
- %status maybe
-
- Determines whether or not the TARG agent's current sprite is mirrored. Returns 0 or 1.
-*/
-void v_MIRA(caosVM* vm) {
-	valid_agent(vm->targ);
-
-	// TODO: correct?
-	SpritePart* p = vm->getCurrentSpritePart();
-	caos_assert(p);
-
-	vm->result.setInt(p->draw_mirrored);
-}
-
-/**
  MIRA (command) mirror_on (integer)
  %status maybe
 
@@ -1490,13 +1474,73 @@ void c_MIRA(caosVM* vm) {
 	VM_PARAM_INTEGER(mirror_on)
 
 	valid_agent(vm->targ);
+	caos_assert(mirror_on == 0 || mirror_on == 1);
+	vm->targ->draw_mirrored = mirror_on;
+}
 
-	// TODO: what does 'current sprite' mean?
-	// TODO: correct?
-	SpritePart* p = vm->getCurrentSpritePart();
-	caos_assert(p);
+/**
+ MIRA (integer)
+ %status maybe
 
-	p->draw_mirrored = mirror_on;
+ Determines whether or not the TARG agent's current sprite is mirrored. Returns 0 or 1.
+*/
+void v_MIRA(caosVM* vm) {
+	valid_agent(vm->targ);
+	vm->result.setInt(vm->targ->draw_mirrored);
+}
+
+/**
+ FLIP (command) onoroff (integer)
+ %status stub
+ %variants sm
+
+ Tell the agent to draw the current sprite vertically flipped.
+*/
+void c_FLIP(caosVM* vm) {
+	VM_PARAM_INTEGER(onoroff);
+	valid_agent(vm->targ);
+	caos_assert(onoroff == 0 || onoroff == 1);
+	// TODO (this isn't actually used anywhere, afaict)
+}
+
+/**
+ FLIP (integer)
+ %status stub
+ %variants sm
+
+ Is the current sprite for this agent vertically flipped?
+*/
+void v_FLIP(caosVM* vm) {
+	valid_agent(vm->targ);
+	// TODO (this isn't actually used anywhere, afaict)
+	vm->result.setInt(0);
+}
+
+/**
+ ROTA (command) angle (integer)
+ %status stub
+ %variants sm
+
+ Tell the agent to draw the current sprite rotated clockwise by the given angle a value between 0 and 360.
+*/
+void c_ROTA(caosVM* vm) {
+	VM_PARAM_INTEGER(angle)
+
+	valid_agent(vm->targ);
+	caos_assert(angle >= 0 && angle <= 360);
+	// TODO: doesn't actually do anything in original engine?
+}
+
+/**
+ ROTA (integer)
+ %status stub
+ %variants sm
+
+ By what angle is the current sprite for this agent rotated returns angle between 0 and 360.
+*/
+void v_ROTA(caosVM* vm) {
+	valid_agent(vm->targ);
+	vm->result.setInt(0);
 }
 
 /**
@@ -1518,7 +1562,7 @@ void v_DISQ(caosVM* vm) {
 }
 
 /**
- ALPH (command) alpha_value (integer) enable (integer)
+ ALPH (command) transparency (integer) enable (integer)
  %status maybe
 
  Sets the degree of alpha blending on the TARG agent, to a value from 0 (solid) to 256 
@@ -1526,27 +1570,16 @@ void v_DISQ(caosVM* vm) {
 */
 void c_ALPH(caosVM* vm) {
 	VM_PARAM_INTEGER(enable)
-	VM_PARAM_INTEGER(alpha_value)
+	VM_PARAM_INTEGER(transparency)
 
-	if (alpha_value < 0)
-		alpha_value = 0;
-	else if (alpha_value > 255)
-		alpha_value = 255;
+	caos_assert(transparency >= 0 && transparency <= 256);
+	if (transparency == 256) {
+		transparency = 255;
+	}
+	caos_assert(enable == 0 || enable == 1);
 
 	valid_agent(vm->targ);
-
-	CompoundAgent* c = dynamic_cast<CompoundAgent*>(vm->targ.get());
-	if (c && vm->part == -1) {
-		for (auto& part : c->parts) {
-			part->has_alpha = enable;
-			part->alpha = alpha_value;
-		}
-	} else {
-		CompoundPart* p = vm->targ->part(vm->part);
-		caos_assert(p);
-		p->has_alpha = enable;
-		p->alpha = alpha_value;
-	}
+	vm->targ->alpha = enable ? 255 - transparency : 255;
 }
 
 /**
@@ -2042,14 +2075,32 @@ void v_XIST(caosVM* vm) {
 }
 
 /**
- SCLE (command) pose (integer) scaleby (integer)
- %status stub
+ SCLE (command) scaleby (float) yesorno (integer)
+ %status done
+ %variants sm
 */
-void c_SCLE(caosVM* vm) {
-	VM_PARAM_INTEGER(scaleby)
+void c_SCLE_sm(caosVM* vm) {
+	VM_PARAM_INTEGER(yesorno)
+	VM_PARAM_FLOAT(scaleby)
+
+	valid_agent(vm->targ);
+
+	caos_assert(yesorno == 0 || yesorno == 1);
+	vm->targ->scle = yesorno ? scaleby : 1.0;
+}
+
+/**
+ SCLE (command) pose(integer) scaleby (float)
+ %status stub
+ %variants cv
+*/
+void c_SCLE_cv(caosVM* vm) {
+	VM_PARAM_FLOAT(scaleby)
 	VM_PARAM_INTEGER(pose)
 
 	valid_agent(vm->targ);
+
+	caos_assert(pose == 1 || pose == -1);
 
 	// TODO
 }
@@ -2057,8 +2108,9 @@ void c_SCLE(caosVM* vm) {
 /**
  STRC (command) width (integer) height (integer) enable (integer)
  %status stub
+ %variants sm
 
- Draw the current agent (or part? don't know) with the given width/height (ie, stretch the sprite). Set enable to 1 to enable, or 0 to disable.
+ Draw the current agent with the given width/height (ie, stretch the sprite). Set enable to 1 to enable, or 0 to disable.
 */
 void c_STRC(caosVM* vm) {
 	VM_PARAM_INTEGER(enable)
@@ -2066,8 +2118,11 @@ void c_STRC(caosVM* vm) {
 	VM_PARAM_INTEGER(width)
 
 	valid_agent(vm->targ);
-
-	// TODO
+	caos_assert(enable == 0 || enable == 1);
+	// TODO: does this affect agent's width/height?
+	vm->targ->strc = enable;
+	vm->targ->strc_width = width;
+	vm->targ->strc_height = height;
 }
 
 /**
@@ -2178,7 +2233,9 @@ void c_SHAD(caosVM* vm) {
 
 	valid_agent(vm->targ);
 
-	// TODO
+	// TODO: only used by clams in Sea-Monkeys.
+	// Intensity doesn't map directly to alpha 0.0–1.0, maybe 0–0.5?
+	// Needs to either only be on primary part, or all shadows rendered before all parts
 }
 
 /**

src/Backend.h
@@ -32,10 +32,22 @@ using std::shared_ptr;
 
 class creaturesImage;
 
+struct RenderOptions {
+	uint8_t alpha = 255;
+	bool mirror = false;
+	float scale = 1.0;
+	bool override_drawsize = false;
+	int overridden_drawwidth;
+	int overridden_drawheight;
+};
+
 class RenderTarget {
   public:
-	virtual void renderCreaturesImage(creaturesImage& tex, unsigned int frame, int x, int y, uint8_t transparency = 0, bool mirror = false) = 0;
-	virtual void renderCreaturesImage(const std::shared_ptr<creaturesImage>& tex, unsigned int frame, int x, int y, uint8_t transparency = 0, bool mirror = false) = 0;
+	virtual void renderCreaturesImage(creaturesImage& tex, unsigned int frame, int x, int y, RenderOptions options = {}) = 0;
+	void renderCreaturesImage(const std::shared_ptr<creaturesImage>& tex, unsigned int frame, int x, int y, RenderOptions options = {}) {
+		assert(tex.get() != nullptr);
+		renderCreaturesImage(*tex.get(), frame, x, y, options);
+	}
 	virtual void renderLine(int x1, int y1, int x2, int y2, unsigned int colour) = 0;
 	virtual void blitRenderTarget(RenderTarget* src, int x, int y, int w, int h) = 0;
 	virtual unsigned int getWidth() const = 0;

src/CompoundPart.cpp
@@ -86,8 +86,6 @@ CompoundPart::CompoundPart(Agent* p, unsigned int _id, int _x, int _y, int _z)
 	addZOrder();
 	x = _x;
 	y = _y;
-
-	has_alpha = false;
 }
 
 CompoundPart::~CompoundPart() {

commit 977c383b6c5e4f53cf28d9e0d6ea4addb4004304

Make alpha a property of parts, not agents, again
Broken in b4810122 "Implement most direct display commands"

Was causing numerous issues, like a missing DS options panel,
weird transparency on the creature selection screen, hoverdoc
panels disappearing or being almost completely transparent.

src/Agent.h
@@ -219,7 +219,6 @@ class Agent : public std::enable_shared_from_this<Agent> {
 	int size = 0;
 
 	// drawing
-	uint8_t alpha = 255;
 	bool draw_mirrored = false;
 	float scle = 1.0; // Sea-Monkeys only
 	bool strc = false; // Sea-Monkeys only

src/CompoundPart.h
@@ -42,6 +42,8 @@ class CompoundPart : public renderable {
 	unsigned int zorder, id;
 	unsigned int part_sequence_number;
 
+	uint8_t alpha = 255;
+
 	virtual void render(class RenderTarget* renderer, int xoffset, int yoffset);
 	virtual void partRender(class RenderTarget* renderer, int xoffset, int yoffset) = 0;
 	virtual void tick() {}

src/SpritePart.cpp
@@ -52,7 +52,7 @@ void SpritePart::partRender(RenderTarget* renderer, int xoffset, int yoffset) {
 	}
 	assert(getCurrentSprite() < getSprite()->numframes());
 	RenderOptions render_opts;
-	render_opts.alpha = parent->alpha;
+	render_opts.alpha = alpha;
 	render_opts.mirror = parent->draw_mirrored;
 	render_opts.scale = parent->scle;
 	if (parent->strc) {

src/TextEntryPart.cpp
@@ -99,7 +99,7 @@ void TextEntryPart::partRender(RenderTarget* renderer, int xoffset, int yoffset)
 void TextEntryPart::renderCaret(RenderTarget* renderer, int xoffset, int yoffset) {
 	// TODO: fudge xoffset/yoffset as required
 	RenderOptions render_opts;
-	render_opts.alpha = parent->alpha;
+	render_opts.alpha = alpha;
 	renderer->renderCreaturesImage(caretsprite, caretpose, xoffset, yoffset, render_opts);
 }
 

src/TextPart.cpp
@@ -1,6 +1,5 @@
 #include "TextPart.h"
 
-#include "Agent.h"
 #include "Backend.h"
 #include "Engine.h"
 #include "TextEntryPart.h"
@@ -272,7 +271,7 @@ void TextPart::partRender(RenderTarget* renderer, int xoffset, int yoffset, Text
 				continue; // TODO: replace with space or similar?
 			int spriteid = ((unsigned char)lines[i].text[x]) - 32;
 			RenderOptions render_opts;
-			render_opts.alpha = parent->alpha;
+			render_opts.alpha = alpha;
 			renderer->renderCreaturesImage(sprite_to_use, spriteid, somex + currentx, yoff + currenty, render_opts);
 			if ((caretdata) && (caretdata->caretpos == lines[i].offset + x))
 				caretdata->renderCaret(renderer, somex + currentx, yoff + currenty);

src/caos/caosVM_agent.cpp
@@ -1579,9 +1579,19 @@ void c_ALPH(caosVM* vm) {
 		transparency = 255;
 	}
 	caos_assert(enable == 0 || enable == 1);
+	uint8_t alpha = enable ? 255 - transparency : 255;
 
 	valid_agent(vm->targ);
-	vm->targ->alpha = enable ? 255 - transparency : 255;
+	CompoundAgent* c = dynamic_cast<CompoundAgent*>(vm->targ.get());
+	if (vm->part == -1) {
+		for (auto& part : c->parts) {
+			part->alpha = alpha;
+		}
+	} else {
+		CompoundPart* p = vm->targ->part(vm->part);
+		caos_assert(p);
+		p->alpha = alpha;
+	}
 }
 
 /**