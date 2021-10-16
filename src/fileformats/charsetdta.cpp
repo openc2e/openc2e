ffa321f4e47444d82260159e2d3f7eb2bfba425c
Michael Maltese <michaeljosephmaltese@gmail.com>
2021-10-15 17:58:29 -0700

    charsetdta.cpp: support Japanese CHARSET.DTA

--- src/fileformats/charsetdta.cpp
+++ src/fileformats/charsetdta.cpp
@@ -13,35 +13,50 @@
   debugging and ease of use.
 */
 static shared_array<Color> getDefaultCharsetPalette() {
-	static shared_array<Color> s_default_charset_palette;
-	if (!s_default_charset_palette) {
-		s_default_charset_palette = shared_array<Color>(256);
-		s_default_charset_palette[0].r = 0;
-		s_default_charset_palette[0].g = 0;
-		s_default_charset_palette[0].b = 0;
-		s_default_charset_palette[0].a = 0xff;
-		for (int i = 1; i < 256; i++) {
-			s_default_charset_palette[i].r = 0xff;
-			s_default_charset_palette[i].g = 0xff;
-			s_default_charset_palette[i].b = 0xff;
-			s_default_charset_palette[i].a = 0xff;
+	static shared_array<Color> s_palette;
+	if (!s_palette) {
+		s_palette = shared_array<Color>(256);
+
+		s_palette[0].r = 0;
+		s_palette[0].g = 0;
+		s_palette[0].b = 0;
+		s_palette[0].a = 255;
+
+		s_palette[1].r = 255;
+		s_palette[1].g = 255;
+		s_palette[1].b = 255;
+		s_palette[1].a = 255;
+
+		s_palette[2].r = 128;
+		s_palette[2].g = 128;
+		s_palette[2].b = 128;
+		s_palette[2].a = 255;
+
+		for (int i = 3; i < 256; ++i) {
+			s_palette[i].r = 0;
+			s_palette[i].g = 0;
+			s_palette[i].b = 128;
+			s_palette[i].a = 255;
 		}
+
 		// nice background color used by spritedumper
-		s_default_charset_palette[100].r = 112;
-		s_default_charset_palette[100].g = 164;
-		s_default_charset_palette[100].b = 236;
+		s_palette[100].r = 112;
+		s_palette[100].g = 164;
+		s_palette[100].b = 236;
 	}
-	return s_default_charset_palette;
+	return s_palette;
 }
 
 /*
  Creatures 1 and 2 define their fonts in a custom file format, used by the
- CHARSET.DTA and EuroCharset.dta files. CHARSET.DTA covers the 128 ASCII
- characters, and EuroCharset.dta covers the 256 Windows-1252 characters.
+ CHARSET.DTA and EuroCharset.dta files. CHARSET.DTA covers 128 ASCII
+ or Windows-932 characters (for Japanese), and EuroCharset.dta covers the 256
+ Windows-1252 characters.
  
- The file format starts with either 128 or 256 72-byte blocks of character data.
- Within the character data, each row is padded out to 6 bytes, and each character
- is 12 pixels high.
+ The file format starts with either 72-byte blocks of character data for each
+ character (132-byte blocks for the Japanese version). Within the character data,
+ each row is padded out to 6 bytes (11 bytes for the Japanese Version), and each
+ character is 12 pixels high.
  
  After the character data, there are either 128 or 256 2-byte integers that
  define the character widths.
@@ -49,26 +64,29 @@ static shared_array<Color> getDefaultCharsetPalette() {
 MultiImage ReadCharsetDtaFile(std::istream& in) {
 	std::vector<uint8_t> filedata{std::istreambuf_iterator<char>(in), {}};
 
-	if (!(filedata.size() == 9472 || filedata.size() == 18944)) {
-		throw creaturesException("Expected size of charset.dta file to be 9472 or 18944 - got " + std::to_string(filedata.size()));
+	if (!(filedata.size() == 9472 || filedata.size() == 17152 || filedata.size() == 18944)) {
+		throw creaturesException("Expected size of charset.dta file to be 9472, 17152, 18944 - got " + std::to_string(filedata.size()));
 	}
-	const unsigned int num_characters = filedata.size() / 74;
+
+	const unsigned int char_width = filedata.size() == 17152 ? 11 : 6;
+	const unsigned int char_height = 12;
+	const unsigned int char_length = char_width * char_height;
+	const unsigned int num_characters = filedata.size() / (char_length + 2);
 
 	MultiImage images(num_characters);
 	for (unsigned int i = 0; i < num_characters; i++) {
-		const unsigned int char_width = read16le(filedata.data() + num_characters * 72 + i * 2);
-		const unsigned int char_height = 12;
+		const unsigned int actual_width = read16le(filedata.data() + num_characters * char_length + i * 2);
 
-		images[i].width = char_width;
+		images[i].width = actual_width;
 		images[i].height = char_height;
 		images[i].format = if_index8;
 		images[i].palette = getDefaultCharsetPalette();
-		images[i].data = shared_array<uint8_t>(char_height * char_width);
+		images[i].data = shared_array<uint8_t>(char_height * actual_width);
 		for (unsigned int row = 0; row < char_height; ++row) {
 			memcpy(
-				images[i].data.data() + row * char_width,
-				filedata.data() + i * 72 + row * 6,
-				char_width);
+				images[i].data.data() + row * actual_width,
+				filedata.data() + i * char_length + row * char_width,
+				actual_width);
 		}
 	}
 	return images;
