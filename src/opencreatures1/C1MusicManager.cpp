d6afbac7f94a01c33582005510fc31fcc77537aa
Michael Maltese <michaeljosephmaltese@gmail.com>
2022-02-06 14:14:56 -0800

    fix lint

--- src/opencreatures1/C1MusicManager.cpp
+++ src/opencreatures1/C1MusicManager.cpp
@@ -1,7 +1,7 @@
 #include "C1MusicManager.h"
 
-#include "openc2e-audiobackend/AudioBackend.h"
 #include "PathManager.h"
+#include "openc2e-audiobackend/AudioBackend.h"
 
 #include <fmt/core.h>
 
@@ -13,9 +13,9 @@ using namespace std::chrono;
 
 static constexpr int MS_PER_TICK = 100;
 
-C1MusicManager::C1MusicManager( std::shared_ptr<PathManager> path_manager, std::shared_ptr<AudioBackend> backend)
+C1MusicManager::C1MusicManager(std::shared_ptr<PathManager> path_manager, std::shared_ptr<AudioBackend> backend)
 	: m_backend(backend), m_path_manager(path_manager) {
-        m_timepoint_to_play_next_sound = c1clock::now() + milliseconds(20 * MS_PER_TICK);
+	m_timepoint_to_play_next_sound = c1clock::now() + milliseconds(20 * MS_PER_TICK);
 }
 
 C1MusicManager::~C1MusicManager() {
@@ -41,21 +41,20 @@ void C1MusicManager::setMuted(bool muted) {
 
 
 void C1MusicManager::updateVolumes() {
-    // reduce overall volume of clips to 80%
+	// reduce overall volume of clips to 80%
 	m_backend->setChannelVolume(m_audio_channel, m_muted ? 0 : m_volume * 0.8);
 }
 
 void C1MusicManager::update() {
 	// play C1 music
-    if (m_timepoint_to_play_next_sound <= c1clock::now() && m_backend->getChannelState(m_audio_channel) == AUDIO_STOPPED) {
-        auto sounds = m_path_manager->find_path_wildcard(PATH_TYPE_SOUND, "MU*.wav");
+	if (m_timepoint_to_play_next_sound <= c1clock::now() && m_backend->getChannelState(m_audio_channel) == AUDIO_STOPPED) {
+		auto sounds = m_path_manager->find_path_wildcard(PATH_TYPE_SOUND, "MU*.wav");
 		if (sounds.size()) {
 			m_audio_channel = m_backend->playClip(sounds[rand() % sounds.size()]);
 		}
 		auto ticks_until_next_sound = 50 + (rand() % 50);
-        m_timepoint_to_play_next_sound = c1clock::now() + milliseconds(MS_PER_TICK * ticks_until_next_sound);
-        
-    }
+		m_timepoint_to_play_next_sound = c1clock::now() + milliseconds(MS_PER_TICK * ticks_until_next_sound);
+	}
 	// update volumes based on new volumes, muting, etc
 	updateVolumes();
 }
