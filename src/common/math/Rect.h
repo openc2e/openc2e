ced59474df7eff601ce435ef0885281b2843b520
Michael Maltese <michaeljosephmaltese@gmail.com>
2022-12-27 12:26:52 -0500

    Rect: set default values, add equality operators

--- src/common/math/Rect.h
+++ src/common/math/Rect.h
@@ -3,11 +3,18 @@
 #include <stdint.h>
 
 struct Rect {
-	int32_t x;
-	int32_t y;
-	int32_t width;
-	int32_t height;
+	int32_t x = 0;
+	int32_t y = 0;
+	int32_t width = 0;
+	int32_t height = 0;
 
 	int32_t right() const { return x + width; }
 	int32_t bottom() const { return y + height; }
+
+	bool operator==(const Rect& other) const {
+		return x == other.x && y == other.y && width == other.width && height == other.height;
+	}
+	bool operator!=(const Rect& other) const {
+		return !(*this == other);
+	}
 };
