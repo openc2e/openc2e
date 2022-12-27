2ba426747a1a9b00a0af0364c38df477adb4fc9e
Michael Maltese <michaeljosephmaltese@gmail.com>
2022-12-27 16:10:47 -0500

    Rect: has_point

--- src/common/math/Rect.h
+++ src/common/math/Rect.h
@@ -11,6 +11,10 @@ struct Rect {
 	int32_t right() const { return x + width; }
 	int32_t bottom() const { return y + height; }
 
+	bool has_point(int32_t pointx, int32_t pointy) const {
+		return pointx >= x && pointx < right() && pointy >= y && pointy < bottom();
+	}
+
 	bool operator==(const Rect& other) const {
 		return x == other.x && y == other.y && width == other.width && height == other.height;
 	}
