8e937515e44105e0b56c68312b3b35958f67aea8
Michael Maltese <michaeljosephmaltese@gmail.com>
2023-04-16 14:27:23 -0400

    StaticVector: clean up implementation

--- src/common/StaticVector.h
+++ src/common/StaticVector.h
@@ -2,7 +2,7 @@
 
 #include "common/NumericCast.h"
 
-#include <exception>
+#include <stdexcept>
 
 template <typename T, size_t N>
 class StaticVector {
@@ -14,19 +14,19 @@ class StaticVector {
 		*this = other;
 	}
 
+	StaticVector(StaticVector&& other)
+		: StaticVector() {
+		*this = std::move(other);
+	}
+
 	StaticVector& operator=(const StaticVector& other) {
 		// TODO: call copy assignment on items that exist
 		clear();
 		size_ = other.size_;
 		for (size_t i = 0; i < size_; ++i) {
-			new (data() + i) T(other[i]);
+			new (&data_[i]) T(other[i]);
 		}
 		return *this;
-	}
-
-	StaticVector(StaticVector&& other)
-		: StaticVector() {
-		*this = std::move(other);
 	}
 
 	StaticVector& operator=(StaticVector&& other) {
@@ -34,7 +34,7 @@ class StaticVector {
 		clear();
 		size_ = other.size_;
 		for (size_t i = 0; i < size_; ++i) {
-			new (data() + i) T(std::move(other[i]));
+			new (&data_[i]) T(std::move(other[i]));
 		}
 		other.clear();
 		return *this;
@@ -45,45 +45,57 @@ class StaticVector {
 	}
 
 	void push_back(const T& t) {
-		emplace_back(t);
-	}
-
-	void push_back(T&& t) {
-		emplace_back(std::move(t));
-	}
-
-	template <typename... Args>
-	void emplace_back(Args&&... args) {
 		if (size_ >= N) {
-			// whoops
-			std::terminate();
+			throw std::length_error("StaticVector push_back exceeds capacity");
 		}
-		new (data() + size_) T(std::forward<Args>(args)...);
-		size_++;
-	}
-
-	T& operator[](size_t i) {
-		if (size_ >= N) {
-			// whoops
-			std::terminate();
-		}
-		return *(data() + i);
-	}
-
-	const T& operator[](size_t i) const {
-		if (size_ >= N) {
-			// whoops
-			std::terminate();
-		}
-		return *(data() + i);
+		new (&data_[size_]) T(t);
+		size_ += 1;
 	}
 
 	T& operator[](int32_t i) {
-		return (*this)[numeric_cast<size_t>(i)];
+		return const_cast<T&>(const_cast<const StaticVector&>(*this)[i]);
 	}
 
 	const T& operator[](int32_t i) const {
-		return (*this)[numeric_cast<size_t>(i)];
+		if (size_ >= N) {
+			throw std::length_error("StaticVector operator[] exceeds capacity");
+		}
+		if (i >= ssize() || i < 0) {
+			throw std::out_of_range("StaticVector operator[] out of range");
+		}
+		return data_[i];
+	}
+
+	T& operator[](size_t i) {
+		return (*this)[numeric_cast<int32_t>(i)];
+	}
+
+	const T& operator[](size_t i) const {
+		return (*this)[numeric_cast<int32_t>(i)];
+	}
+
+	T* begin() {
+		return data_;
+	}
+
+	const T* begin() const {
+		return data_;
+	}
+
+	T* end() {
+		return &data_[size_];
+	}
+
+	const T* end() const {
+		return &data_[size_];
+	}
+
+	T& back() {
+		return data_[size_ - 1];
+	}
+
+	const T& back() const {
+		return data_[size_ - 1];
 	}
 
 	void clear() {
@@ -92,45 +104,20 @@ class StaticVector {
 		}
 	}
 
-	T* begin() {
-		return data();
-	}
-
-	const T* begin() const {
-		return data();
-	}
-
-	T* end() {
-		return data() + size_;
-	}
-
-	const T* end() const {
-		return data() + size_;
-	}
-
-	T& back() {
-		return *(data() + size_ - 1);
-	}
-
-	const T& back() const {
-		return *(data() + size_ - 1);
-	}
-
 	void pop_back() {
 		if (size_ == 0) {
-			// whoops
-			std::terminate();
+			throw std::out_of_range("StaticVector pop_back() empty vector");
 		}
-		(data() + size_ - 1)->~T();
+		data_[size_ - 1].~T();
 		size_ -= 1;
 	}
 
 	T* data() {
-		return &data_[0].value;
+		return data_;
 	}
 
 	const T* data() const {
-		return &data_[0].value;
+		return data_;
 	}
 
 	size_t size() const {
@@ -138,7 +125,7 @@ class StaticVector {
 	}
 
 	int32_t ssize() const {
-		return numeric_cast<int32_t>(size());
+		return numeric_cast<int32_t>(size_);
 	}
 
 	size_t capacity() const {
@@ -146,9 +133,8 @@ class StaticVector {
 	}
 
   private:
-	union U {
-		U() {}
-		T value;
-	} data_[N];
+	union {
+		T data_[N];
+	};
 	size_t size_ = 0;
 };
