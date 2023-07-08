43aeb7c6485c1109394ce51feb92dd8c6d92d3a1
Michael Maltese <michaeljosephmaltese@gmail.com>
2023-07-08 15:46:09 -0400

    PraySourceParser: don't rely on SimpleLexer

--- src/fileformats/PraySourceParser.cpp
+++ src/fileformats/PraySourceParser.cpp
@@ -1,15 +1,206 @@
 #include "PraySourceParser.h"
 
-#include "common/SimpleLexer.h"
+#include "common/Ascii.h"
+#include "common/NumericCast.h"
 #include "common/encoding.h"
 
 #include <cassert>
 #include <fmt/format.h>
-#include <iostream>
-#include <map>
 #include <string>
+#include <vector>
 
 using namespace PraySourceParser;
+
+enum PrayTokenType {
+	PRAY_BAREWORD,
+	PRAY_STRING,
+	PRAY_INTEGER,
+	PRAY_ATSIGN,
+	PRAY_COMMENT,
+	PRAY_WHITESPACE,
+	PRAY_NEWLINE,
+	PRAY_ERROR,
+	PRAY_EOI
+};
+
+struct PrayToken {
+	PrayToken()
+		: type(PRAY_ERROR), lineno(-1) {}
+	PrayToken(PrayTokenType type_, int lineno_, std::string raw_value_)
+		: type(type_), lineno(lineno_), raw_value(raw_value_) {}
+	PrayTokenType type;
+	int lineno;
+	std::string raw_value;
+
+	std::string value() const;
+	int integer_value() const;
+
+	bool is_one_of(std::initializer_list<PrayTokenType>) const;
+	bool is_integer() const;
+	bool is_atsign() const;
+
+	std::string repr() const;
+	const char* c_str() const;
+};
+
+
+static char unescape(char c) {
+	switch (c) {
+		case 'n': return '\n';
+		case 'r': return '\r';
+		case 't': return '\t';
+		default: return c;
+	}
+}
+
+std::string PrayToken::value() const {
+	if (type == PRAY_STRING) {
+		// remove double quotes and unescape characters
+		std::string newvalue;
+		for (size_t i = 1; i < raw_value.size() - 1; ++i) {
+			if (raw_value[i] == '\\') {
+				++i;
+				newvalue += unescape(raw_value[i]);
+			} else {
+				newvalue += raw_value[i];
+			}
+		}
+		return newvalue;
+	}
+	return raw_value;
+}
+
+int PrayToken::integer_value() const {
+	assert(is_integer());
+	return std::stoi(raw_value);
+}
+
+bool PrayToken::is_one_of(std::initializer_list<PrayTokenType> list) const {
+	for (auto t : list) {
+		if (type == t) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool PrayToken::is_integer() const {
+	return type == PRAY_INTEGER;
+}
+
+bool PrayToken::is_atsign() const {
+	return type == PRAY_ATSIGN;
+}
+
+std::string PrayToken::repr() const {
+	// TODO: escape characters
+	return std::string(c_str()) + " " + raw_value;
+}
+
+const char* PrayToken::c_str() const {
+	switch (type) {
+		case PRAY_BAREWORD: return "string";
+		case PRAY_STRING: return "string";
+		case PRAY_INTEGER: return "integer";
+		case PRAY_ATSIGN: return "atsign";
+		case PRAY_COMMENT: return "comment";
+		case PRAY_WHITESPACE: return "whitespace";
+		case PRAY_NEWLINE: return "newline";
+		case PRAY_ERROR: return "lexer error";
+		case PRAY_EOI: return "<eoi>";
+	}
+}
+
+std::vector<PrayToken> praylex(const char* p) {
+	std::vector<PrayToken> v;
+	const char* basep;
+	assert(p);
+	int yylineno = 1;
+	int baseyylineno;
+
+#define push_value(type) \
+	v.push_back(PrayToken(type, baseyylineno, std::string(basep, numeric_cast<std::string::size_type>(p - basep)))); \
+	goto start;
+
+#define push_value_and_error(type) \
+	v.push_back(PrayToken(type, baseyylineno, std::string(basep, numeric_cast<std::string::size_type>(p - basep)))); \
+	v.push_back(PrayToken(PRAY_ERROR, baseyylineno, "")); \
+	goto start;
+
+start:
+	basep = p;
+	baseyylineno = yylineno;
+
+	if (p[0] == '\0') {
+		goto eoi;
+	} else if (p[0] == '(' && p[1] == '-') {
+		p += 2;
+		// TODO: do these comments allow embedded newlines?
+		while (!(p[0] == '\0' || (p[0] == '-' && p[1] == ')'))) {
+			p++;
+		}
+		if (p[0] == '\0') {
+			push_value_and_error(PRAY_COMMENT);
+		}
+		p += 2;
+		push_value(PRAY_COMMENT);
+	} else if (p[0] == '\n') {
+		p++;
+		yylineno++;
+		push_value(PRAY_NEWLINE);
+	} else if (p[0] == '\r' && p[1] == '\n') {
+		p += 2;
+		yylineno++;
+		push_value(PRAY_NEWLINE);
+	} else if (p[0] == ' ' || p[0] == '\t' || (p[0] == '\r' && p[1] != '\n')) {
+		p++;
+		while (p[0] == ' ' || p[0] == '\t' || (p[0] == '\r' && p[1] != '\n')) {
+			p++;
+		}
+		push_value(PRAY_WHITESPACE);
+	} else if (is_ascii_digit(p[0]) || (p[0] == '-' && is_ascii_digit(p[1]))) {
+		// normal numbers
+		p++;
+		while (is_ascii_digit(p[0])) {
+			p++;
+		}
+		push_value(PRAY_INTEGER);
+	} else if (p[0] == '"') {
+		p++;
+		goto str;
+	} else if (p[0] == '@') {
+		p++;
+		push_value(PRAY_ATSIGN);
+	} else if (is_ascii_alpha(p[0])) {
+		p++;
+		while (is_ascii_alpha(p[0])) {
+			p++;
+		}
+		push_value(PRAY_BAREWORD);
+	} else {
+		p++;
+		push_value(PRAY_ERROR);
+	}
+
+str:
+	if (p[0] == '\0' || p[0] == '\r' || p[0] == '\n') {
+		p++;
+		push_value(PRAY_ERROR);
+	} else if (p[0] == '\\') {
+		p += 2;
+		goto str;
+	} else if (p[0] == '"') {
+		p++;
+		push_value(PRAY_STRING);
+	} else {
+		p++;
+		goto str;
+	}
+
+eoi:
+	v.push_back(PrayToken(PRAY_EOI, yylineno, "\0"));
+	return v;
+}
 
 class PraySourceParserImpl {
   public:
@@ -18,35 +209,35 @@ class PraySourceParserImpl {
 	std::vector<PraySourceParser::Event> next();
 
   private:
-	std::vector<SimpleToken> tokens;
+	std::vector<PrayToken> tokens;
 
-	SimpleToken peek_token() const;
+	PrayToken peek_token() const;
 	void next_token();
 	PraySourceParser::Event parse_line();
 	bool parse_some_ws();
 
 	int yylineno = 1;
 	size_t token_p = 0;
-	SimpleToken token;
+	PrayToken token;
 	bool in_group_block = false;
 	std::string last_group_type;
 	std::string last_group_name;
 	bool seen_en_gb = false;
 };
 
-SimpleToken PraySourceParserImpl::peek_token() const {
+PrayToken PraySourceParserImpl::peek_token() const {
 	return tokens[token_p];
 }
 
 void PraySourceParserImpl::next_token() {
 	token = tokens[token_p++];
-	if (token.type == SIMPLE_NEWLINE) {
+	if (token.type == PRAY_NEWLINE) {
 		yylineno++;
 	}
 }
 
 PraySourceParserImpl::PraySourceParserImpl(const char* buf)
-	: tokens(simplelex(buf)), yylineno(1) {
+	: tokens(praylex(buf)), yylineno(1) {
 	next_token();
 }
 
@@ -80,11 +271,11 @@ std::vector<PraySourceParser::Event> PraySourceParserImpl::next() {
 	// skip starting whitespace, empty lines, and initial en-GB
 	while (true) {
 		parse_some_ws();
-		if (!seen_en_gb && token.type == SIMPLE_STRING && token.value() == "en-GB") {
+		if (!seen_en_gb && token.type == PRAY_STRING && token.value() == "en-GB") {
 			seen_en_gb = true;
 			next_token();
 		}
-		if (token.type == SIMPLE_NEWLINE) {
+		if (token.type == PRAY_NEWLINE) {
 			next_token();
 		} else {
 			break;
@@ -95,13 +286,13 @@ std::vector<PraySourceParser::Event> PraySourceParserImpl::next() {
 	}
 
 	// generate synthetic "GroupBlock" lines
-	if (in_group_block && (token.type == SIMPLE_BAREWORD || token.type == SIMPLE_EOI)) {
+	if (in_group_block && (token.type == PRAY_BAREWORD || token.type == PRAY_EOI)) {
 		in_group_block = false;
 		return {GroupBlockEnd{last_group_type, last_group_name}};
 	}
 
 	// parse the actual line
-	if (token.type == SIMPLE_EOI) {
+	if (token.type == PRAY_EOI) {
 		return {};
 	}
 	PraySourceParser::Event result = parse_line();
@@ -111,10 +302,10 @@ std::vector<PraySourceParser::Event> PraySourceParserImpl::next() {
 
 	// parse ending whitespace/newline
 	parse_some_ws();
-	if (token.type == SIMPLE_EOI) {
+	if (token.type == PRAY_EOI) {
 		return {result};
 	}
-	if (token.type != SIMPLE_NEWLINE) {
+	if (token.type != PRAY_NEWLINE) {
 		return {Error{"Expected newline, got " + token.repr()}};
 	}
 	next_token();
@@ -123,11 +314,11 @@ std::vector<PraySourceParser::Event> PraySourceParserImpl::next() {
 }
 
 PraySourceParser::Event PraySourceParserImpl::parse_line() {
-	if (token.type == SIMPLE_ERROR) {
+	if (token.type == PRAY_ERROR) {
 		return Error{token.repr()};
 	}
 
-	if (token.type == SIMPLE_STRING) {
+	if (token.type == PRAY_STRING) {
 		std::string key = token.value();
 		next_token();
 
@@ -136,17 +327,17 @@ PraySourceParser::Event PraySourceParserImpl::parse_line() {
 						 token.repr()};
 		}
 
-		if (token.type == SIMPLE_STRING) {
+		if (token.type == PRAY_STRING) {
 			std::string value = token.value();
 			next_token();
 			return StringTag{key, value};
-		} else if (token.is_symbol('@')) {
+		} else if (token.is_atsign()) {
 			next_token();
 			if (!parse_some_ws()) {
 				return Error{"Expected whitespace after '@', got " +
 							 token.repr()};
 			}
-			if (token.type != SIMPLE_STRING) {
+			if (token.type != PRAY_STRING) {
 				return Error{"Expected string after '@', got " + token.repr()};
 			}
 			std::string value = token.value();
@@ -162,7 +353,7 @@ PraySourceParser::Event PraySourceParserImpl::parse_line() {
 		}
 	}
 
-	if (token.type == SIMPLE_BAREWORD) {
+	if (token.type == PRAY_BAREWORD) {
 		std::string directive = token.value();
 		next_token();
 
@@ -175,7 +366,7 @@ PraySourceParser::Event PraySourceParserImpl::parse_line() {
 						 token.repr()};
 		}
 
-		if (token.type != SIMPLE_BAREWORD) {
+		if (token.type != PRAY_BAREWORD) {
 			return Error{"Expected block type, got " + token.repr()};
 		}
 		std::string type = token.value();
@@ -186,7 +377,7 @@ PraySourceParser::Event PraySourceParserImpl::parse_line() {
 		}
 
 		if (directive == "group") {
-			if (token.type != SIMPLE_STRING) {
+			if (token.type != PRAY_STRING) {
 				return Error{"Expected block name, got " + token.repr()};
 			}
 			std::string name = token.value();
@@ -198,7 +389,7 @@ PraySourceParser::Event PraySourceParserImpl::parse_line() {
 			return GroupBlockStart{type, name};
 
 		} else if (directive == "inline") {
-			if (token.type != SIMPLE_STRING) {
+			if (token.type != PRAY_STRING) {
 				return Error{"Expected block label, got " + token.repr()};
 			}
 			std::string label = token.value();
@@ -207,7 +398,7 @@ PraySourceParser::Event PraySourceParserImpl::parse_line() {
 			if (!parse_some_ws())
 				return Error{""};
 
-			if (token.type != SIMPLE_STRING) {
+			if (token.type != PRAY_STRING) {
 				return Error{"Expected quoted filename, got " + token.repr()};
 			}
 			std::string filename = token.value();
@@ -224,21 +415,10 @@ bool PraySourceParserImpl::parse_some_ws() {
 	// parse whitespace or comment (but not newline)
 	bool parsed_ws = false;
 	while (true) {
-		if (token.type == SIMPLE_WHITESPACE) {
+		if (token.type == PRAY_WHITESPACE) {
 			parsed_ws = true;
 			next_token();
-		} else if (token.is_symbol('(') && peek_token().is_symbol('*')) {
-			token_p += 2;
-			while (true) {
-				if (token.is_symbol('*') && peek_token().is_symbol('}')) {
-					token_p += 2;
-					break;
-				} else if (token.type == SIMPLE_ERROR || token.type == SIMPLE_EOI) {
-					break;
-				} else {
-					next_token();
-				}
-			}
+		} else if (token.type == PRAY_COMMENT) {
 			next_token();
 		} else {
 			break;
