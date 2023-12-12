c2d9c7950654d7ae16727ba546ba5341ecbcdb95
Michael Maltese <michaeljosephmaltese@gmail.com>
2023-12-12 09:04:22 -0500

    SFCSerialization: combine more load/dump logic

--- src/opencreatures1/SFCSerialization.cpp
+++ src/opencreatures1/SFCSerialization.cpp
@@ -15,15 +15,11 @@
 #include <fmt/ranges.h>
 
 struct SFCLoader {
-	SFCLoader(const sfc::SFCFile& sfc_)
+	SFCLoader(sfc::SFCFile& sfc_)
 		: sfc(sfc_) {}
 	void load_everything();
 
-	void load_viewport();
-	void load_map();
 	void load_objects_and_sceneries();
-	void load_scripts();
-	void load_macros();
 
 	ObjectHandle load_object(sfc::ObjectV1* p);
 	std::shared_ptr<sfc::ObjectV1> dump_object(Object*) {
@@ -33,7 +29,7 @@ struct SFCLoader {
 	constexpr bool is_loading() const { return true; }
 	constexpr bool is_storing() const { return false; }
 
-	const sfc::SFCFile& sfc;
+	sfc::SFCFile& sfc;
 	std::map<const sfc::ObjectV1*, ObjectHandle> sfc_object_mapping;
 };
 
@@ -50,118 +46,91 @@ struct SFCSerializer {
 	constexpr bool is_storing() const { return true; }
 };
 
-void SFCLoader::load_everything() {
-	fmt::print("INFO [SFCLoader] Loading map...\n");
-	load_map();
-
-	fmt::print("INFO [SFCLoader] Loading objects and sceneries...\n");
-	load_objects_and_sceneries();
-
-	fmt::print("INFO [SFCLoader] Loading scriptorium...\n");
-	load_scripts();
-
-	fmt::print("INFO [SFCLoader] Loading viewport...\n");
-	load_viewport();
-
-	fmt::print("WARN [SFCLoader] Unsupported: current norn\n");
-	fmt::print("WARN [SFCLoader] Unsupported: favorite places\n");
-	fmt::print("WARN [SFCLoader] Unsupported: speech history\n");
-
-	fmt::print("INFO [SFCLoader] Loading macros...\n");
-	load_macros();
-
-	fmt::print("WARN [SFCLoader] Unsupported: death row objects\n");
-	fmt::print("WARN [SFCLoader] Unsupported: event objects\n");
-	fmt::print("WARN [SFCLoader] Unsupported: current score\n");
-	fmt::print("WARN [SFCLoader] Unsupported: current health\n");
-	fmt::print("WARN [SFCLoader] Unsupported: hatchery eggs\n");
-	fmt::print("WARN [SFCLoader] Unsupported: natural eggs\n");
-	fmt::print("WARN [SFCLoader] Unsupported: dead norns\n");
-	fmt::print("WARN [SFCLoader] Unsupported: live norns\n");
-	fmt::print("WARN [SFCLoader] Unsupported: breeder score\n");
-	fmt::print("WARN [SFCLoader] Unsupported: system tick\n");
-	fmt::print("WARN [SFCLoader] Unsupported: stuffed norns\n");
+template <typename Ctx>
+void serialize_viewport(Ctx&& ctx) {
+	if (ctx.is_storing()) {
+		ctx.sfc.scrollx = g_engine_context.viewport->get_scrollx();
+		ctx.sfc.scrolly = g_engine_context.viewport->get_scrolly();
+	} else {
+		fmt::print("INFO [SFCLoader] Viewport {}, {}\n", ctx.sfc.scrollx, ctx.sfc.scrolly);
+		g_engine_context.viewport->set_scroll_position(ctx.sfc.scrollx, ctx.sfc.scrolly);
+	}
 }
 
-void SFCLoader::load_viewport() {
-	fmt::print("INFO [SFCLoader] Viewport {}, {}\n", sfc.scrollx, sfc.scrolly);
-	g_engine_context.viewport->set_scroll_position(sfc.scrollx, sfc.scrolly);
-}
+template <typename Ctx>
+void serialize_map(Ctx&& ctx) {
+	if (ctx.is_storing()) {
+		auto map = std::make_shared<sfc::MapDataV1>();
+		map->background = std::make_shared<sfc::CGalleryV1>();
+		map->background->filename = g_engine_context.map->get_background().name;
+		map->background->absolute_base = g_engine_context.map->get_background().absolute_base;
+		map->background->refcount = 1;
 
+		// don't use sfc_dump_gallery, because map galleries are stitched together unlike normal sprites.
+		// we hardcode the tile sizes here since they're always the same, instead of storing the original
+		// sizes on the gallery.
+		for (auto offset : g_engine_context.map->get_background().offsets) {
+			map->background->images.emplace_back();
+			auto& image = map->background->images.back();
+			image.parent = map->background.get();
+			image.status = 0; // seems to work???
+			image.width = 144;
+			image.height = 150;
+			image.offset = offset;
+		}
 
-void SFCLoader::load_map() {
-	if (sfc.map->unused_is_wrappable != 0) {
-		fmt::print("WARN [SFCLoader] unused_is_wrappable = {}, expected 0\n", sfc.map->unused_is_wrappable);
+		fmt::print("INFO [SFCWriter] Writing rooms...\n");
+		for (auto& r : g_engine_context.map->get_rooms()) {
+			sfc::RoomV1 room;
+			room.left = r.left;
+			room.top = r.top;
+			room.right = r.right;
+			room.bottom = r.bottom;
+			room.type = r.type;
+			map->rooms.push_back(room);
+		}
+
+		fmt::print("INFO [SFCWriter] Writing groundlevel...\n");
+		map->groundlevel = g_engine_context.map->get_groundlevel();
+
+		ctx.sfc.map = map;
+	} else {
+		if (ctx.sfc.map->unused_is_wrappable != 0) {
+			fmt::print("WARN [SFCLoader] unused_is_wrappable = {}, expected 0\n", ctx.sfc.map->unused_is_wrappable);
+		}
+		fmt::print("WARN [SFCLoader] Unsupported: time_of_day = {}\n", ctx.sfc.map->time_of_day);
+
+		fmt::print("INFO [SFCLoader] Loading background = {}.spr\n", ctx.sfc.map->background->filename);
+		auto background = g_engine_context.images->get_image(
+			ctx.sfc.map->background->filename,
+			ctx.sfc.map->background->absolute_base,
+			numeric_cast<int32_t>(ctx.sfc.map->background->images.size()),
+			static_cast<ImageManager::ImageType>(ImageManager::IMAGE_SPR | ImageManager::IMAGE_IS_BACKGROUND));
+		if (ctx.sfc.map->background->absolute_base != 0) {
+			fmt::print("WARN [SFCLoader] map background absolute base = {}, expected 0", ctx.sfc.map->background->absolute_base);
+		}
+		// TODO: do any C1 metarooms have non-standard sizes?
+		if (background.width(0) != CREATURES1_WORLD_WIDTH || background.height(0) != CREATURES1_WORLD_HEIGHT) {
+			throw Exception(fmt::format("Expected Creatures 1 background size to be 8352x1200 but got {}x{}", background.width(0), background.height(0)));
+		}
+		g_engine_context.map->set_background(background);
+
+		fmt::print("INFO [SFCLoader] Loading rooms...\n");
+		for (auto& r : ctx.sfc.map->rooms) {
+			Room room;
+			room.left = r.left;
+			room.top = r.top;
+			room.right = r.right;
+			room.bottom = r.bottom;
+			room.type = r.type;
+			g_engine_context.map->add_room(std::move(room));
+		}
+
+		fmt::print("INFO [SFCLoader] Loading groundlevel...\n");
+		g_engine_context.map->set_groundlevel(ctx.sfc.map->groundlevel);
+
+		fmt::print("WARN [SFCLoader] Unsupported: bacteria\n");
 	}
-	fmt::print("WARN [SFCLoader] Unsupported: time_of_day = {}\n", sfc.map->time_of_day);
-
-	fmt::print("INFO [SFCLoader] Loading background = {}.spr\n", sfc.map->background->filename);
-	auto background = g_engine_context.images->get_image(
-		sfc.map->background->filename,
-		sfc.map->background->absolute_base,
-		numeric_cast<int32_t>(sfc.map->background->images.size()),
-		static_cast<ImageManager::ImageType>(ImageManager::IMAGE_SPR | ImageManager::IMAGE_IS_BACKGROUND));
-	if (sfc.map->background->absolute_base != 0) {
-		fmt::print("WARN [SFCLoader] map background absolute base = {}, expected 0", sfc.map->background->absolute_base);
-	}
-	// TODO: do any C1 metarooms have non-standard sizes?
-	if (background.width(0) != CREATURES1_WORLD_WIDTH || background.height(0) != CREATURES1_WORLD_HEIGHT) {
-		throw Exception(fmt::format("Expected Creatures 1 background size to be 8352x1200 but got {}x{}", background.width(0), background.height(0)));
-	}
-	g_engine_context.map->set_background(background);
-
-	fmt::print("INFO [SFCLoader] Loading rooms...\n");
-	for (auto& r : sfc.map->rooms) {
-		Room room;
-		room.left = r.left;
-		room.top = r.top;
-		room.right = r.right;
-		room.bottom = r.bottom;
-		room.type = r.type;
-		g_engine_context.map->add_room(std::move(room));
-	}
-
-	fmt::print("INFO [SFCLoader] Loading groundlevel...\n");
-	g_engine_context.map->set_groundlevel(sfc.map->groundlevel);
-
-	fmt::print("WARN [SFCLoader] Unsupported: bacteria\n");
-}
-
-static std::shared_ptr<sfc::MapDataV1> sfc_dump_map() {
-	auto map = std::make_shared<sfc::MapDataV1>();
-	map->background = std::make_shared<sfc::CGalleryV1>();
-	map->background->filename = g_engine_context.map->get_background().name;
-	map->background->absolute_base = g_engine_context.map->get_background().absolute_base;
-	map->background->refcount = 1;
-
-	// don't use sfc_dump_gallery, because map galleries are stitched together unlike normal sprites.
-	// we hardcode the tile sizes here since they're always the same, instead of storing the original
-	// sizes on the gallery.
-	for (auto offset : g_engine_context.map->get_background().offsets) {
-		map->background->images.emplace_back();
-		auto& image = map->background->images.back();
-		image.parent = map->background.get();
-		image.status = 0; // seems to work???
-		image.width = 144;
-		image.height = 150;
-		image.offset = offset;
-	}
-
-	fmt::print("INFO [SFCWriter] Writing rooms...\n");
-	for (auto& r : g_engine_context.map->get_rooms()) {
-		sfc::RoomV1 room;
-		room.left = r.left;
-		room.top = r.top;
-		room.right = r.right;
-		room.bottom = r.bottom;
-		room.type = r.type;
-		map->rooms.push_back(room);
-	}
-
-	fmt::print("INFO [SFCWriter] Writing groundlevel...\n");
-	map->groundlevel = g_engine_context.map->get_groundlevel();
-
-	return map;
 }
 
 static std::shared_ptr<sfc::CGalleryV1> sfc_dump_gallery(const ImageGallery& gallery) {
@@ -591,51 +560,86 @@ void SFCLoader::load_objects_and_sceneries() {
 	fmt::print("INFO [SFCLoader] Loaded {} sceneries\n", sfc.sceneries.size());
 }
 
-void SFCLoader::load_scripts() {
-	for (auto& s : sfc.scripts) {
-		g_engine_context.scriptorium->add(s.family, s.genus, s.species, s.eventno, s.text);
-	}
-}
-
-void sfc_dump_scripts(sfc::SFCFile& sfc) {
-	for (auto& p : *g_engine_context.scriptorium) {
-		sfc::ScriptV1 script;
-		script.family = p.first.family;
-		script.genus = p.first.genus;
-		script.species = p.first.species;
-		script.eventno = p.first.eventno;
-		script.text = p.second;
-		sfc.scripts.push_back(script);
-	}
-}
-
-void SFCLoader::load_macros() {
-	for (const auto& m : sfc.macros) {
-		Macro macro;
-		macro.selfdestruct = m->selfdestruct;
-		macro.inst = m->inst;
-		macro.script = m->script;
-		macro.ip = m->ip;
-		for (size_t i = 0; i < m->sp; ++i) {
-			macro.stack.push_back(m->stack[i]);
+template <typename Ctx>
+void serialize_scripts(Ctx&& ctx) {
+	if (ctx.is_storing()) {
+		for (auto& p : *g_engine_context.scriptorium) {
+			sfc::ScriptV1 script;
+			script.family = p.first.family;
+			script.genus = p.first.genus;
+			script.species = p.first.species;
+			script.eventno = p.first.eventno;
+			script.text = p.second;
+			ctx.sfc.scripts.push_back(script);
 		}
-		macro.vars = m->vars;
-		macro.ownr = load_object(m->ownr);
-		macro.from = load_object(m->from);
-		macro.exec = load_object(m->exec);
-		macro.targ = load_object(m->targ);
-		macro._it_ = load_object(m->_it_);
-		macro.part = m->part;
-		macro.subroutine_label = m->subroutine_label;
-		macro.subroutine_address = m->subroutine_address;
-		macro.wait = m->wait;
+	} else {
+		for (auto& s : ctx.sfc.scripts) {
+			g_engine_context.scriptorium->add(s.family, s.genus, s.species, s.eventno, s.text);
+		}
+	}
+}
 
-		g_engine_context.macros->add(macro);
+template <typename Ctx>
+void serialize_macros(Ctx&& ctx) {
+	if (ctx.is_storing()) {
+		for (auto& m : *g_engine_context.macros) {
+			auto mac = std::make_shared<sfc::MacroV1>();
+			mac->selfdestruct = m.selfdestruct;
+			mac->inst = m.inst;
+			mac->script = m.script;
+			mac->ip = m.ip;
+			for (size_t i = 0; i < m.stack.size(); ++i) {
+				mac->stack[i] = m.stack[i];
+			}
+			mac->sp = numeric_cast<uint32_t>(m.stack.size());
+			mac->vars = m.vars;
+			mac->ownr = ctx.dump_object(g_engine_context.objects->try_get(m.ownr)).get();
+			mac->from = ctx.dump_object(g_engine_context.objects->try_get(m.from)).get();
+			mac->exec = ctx.dump_object(g_engine_context.objects->try_get(m.exec)).get();
+			mac->targ = ctx.dump_object(g_engine_context.objects->try_get(m.targ)).get();
+			mac->_it_ = ctx.dump_object(g_engine_context.objects->try_get(m._it_)).get();
+			mac->part = m.part;
+			mac->subroutine_label = m.subroutine_label;
+			mac->subroutine_address = m.subroutine_address;
+			mac->wait = m.wait;
+
+			if (m.destroy_as_soon_as_possible) {
+				printf("WARN [SFCWriter] Macro.destroy_as_soon_as_possible unsupported\n");
+			}
+			if (m.enum_result.size()) {
+				printf("ERROR [SFCWriter] Macro.enum_result unsupported\n");
+			}
+
+			ctx.sfc.macros.push_back(mac);
+		}
+	} else {
+		for (const auto& m : ctx.sfc.macros) {
+			Macro macro;
+			macro.selfdestruct = m->selfdestruct;
+			macro.inst = m->inst;
+			macro.script = m->script;
+			macro.ip = m->ip;
+			for (size_t i = 0; i < m->sp; ++i) {
+				macro.stack.push_back(m->stack[i]);
+			}
+			macro.vars = m->vars;
+			macro.ownr = ctx.load_object(m->ownr);
+			macro.from = ctx.load_object(m->from);
+			macro.exec = ctx.load_object(m->exec);
+			macro.targ = ctx.load_object(m->targ);
+			macro._it_ = ctx.load_object(m->_it_);
+			macro.part = m->part;
+			macro.subroutine_label = m->subroutine_label;
+			macro.subroutine_address = m->subroutine_address;
+			macro.wait = m->wait;
+
+			g_engine_context.macros->add(macro);
+		}
 	}
 }
 
 void sfc_load_everything(const sfc::SFCFile& sfc) {
-	SFCLoader loader(sfc);
+	SFCLoader loader(const_cast<sfc::SFCFile&>(sfc));
 	loader.load_everything();
 }
 
@@ -687,46 +691,12 @@ std::shared_ptr<sfc::ObjectV1> SFCSerializer::dump_object(Object* p) {
 };
 
 
-static void sfc_dump_objects_and_sceneries_and_macros(SFCSerializer& ctx) {
+static void sfc_dump_objects_and_sceneries(SFCSerializer& ctx) {
 	// so, similarly to _loading_ from the SFC, this is tricky since SFC objects
 	// can encode points to other objects. we keep a map of all ObjectHandles we've
 	// seen so far, and convert ObjectHandles / copy them to the SFCFile on the fly.
-
-	fmt::print("INFO [SFCWriter] Writing objects and sceneries...\n");
 	for (auto* p : *g_engine_context.objects) {
 		ctx.dump_object(p);
-	}
-
-	fmt::print("INFO [SFCWriter] Writing macros...\n");
-	for (auto& m : *g_engine_context.macros) {
-		auto mac = std::make_shared<sfc::MacroV1>();
-		mac->selfdestruct = m.selfdestruct;
-		mac->inst = m.inst;
-		mac->script = m.script;
-		mac->ip = m.ip;
-		for (size_t i = 0; i < m.stack.size(); ++i) {
-			mac->stack[i] = m.stack[i];
-		}
-		mac->sp = numeric_cast<uint32_t>(m.stack.size());
-		mac->vars = m.vars;
-		mac->ownr = ctx.dump_object(g_engine_context.objects->try_get(m.ownr)).get();
-		mac->from = ctx.dump_object(g_engine_context.objects->try_get(m.from)).get();
-		mac->exec = ctx.dump_object(g_engine_context.objects->try_get(m.exec)).get();
-		mac->targ = ctx.dump_object(g_engine_context.objects->try_get(m.targ)).get();
-		mac->_it_ = ctx.dump_object(g_engine_context.objects->try_get(m._it_)).get();
-		mac->part = m.part;
-		mac->subroutine_label = m.subroutine_label;
-		mac->subroutine_address = m.subroutine_address;
-		mac->wait = m.wait;
-
-		if (m.destroy_as_soon_as_possible) {
-			printf("WARN [SFCWriter] Macro.destroy_as_soon_as_possible unsupported\n");
-		}
-		if (m.enum_result.size()) {
-			printf("ERROR [SFCWriter] Macro.enum_result unsupported\n");
-		}
-
-		ctx.sfc.macros.push_back(mac);
 	}
 }
 
@@ -734,16 +704,52 @@ sfc::SFCFile sfc_dump_everything() {
 	SFCSerializer ctx;
 
 	fmt::print("INFO [SFCWriter] Writing map...\n");
-	ctx.sfc.map = sfc_dump_map();
+	serialize_map(ctx);
 
-	sfc_dump_objects_and_sceneries_and_macros(ctx);
+	fmt::print("INFO [SFCWriter] Writing objects and sceneries...\n");
+	sfc_dump_objects_and_sceneries(ctx);
+
+	fmt::print("INFO [SFCWriter] Writing macros...\n");
+	serialize_macros(ctx);
 
 	fmt::print("INFO [SFCWriter] Writing scriptorium...\n");
-	sfc_dump_scripts(ctx.sfc);
+	serialize_scripts(ctx);
 
 	fmt::print("INFO [SFCWriter] Writing viewport...\n");
-	ctx.sfc.scrollx = g_engine_context.viewport->get_scrollx();
-	ctx.sfc.scrolly = g_engine_context.viewport->get_scrolly();
+	serialize_viewport(ctx);
 
 	return ctx.sfc;
+}
+
+void SFCLoader::load_everything() {
+	fmt::print("INFO [SFCLoader] Loading map...\n");
+	serialize_map(*this);
+
+	fmt::print("INFO [SFCLoader] Loading objects and sceneries...\n");
+	load_objects_and_sceneries();
+
+	fmt::print("INFO [SFCLoader] Loading scriptorium...\n");
+	serialize_scripts(*this);
+
+	fmt::print("INFO [SFCLoader] Loading viewport...\n");
+	serialize_viewport(*this);
+
+	fmt::print("WARN [SFCLoader] Unsupported: current norn\n");
+	fmt::print("WARN [SFCLoader] Unsupported: favorite places\n");
+	fmt::print("WARN [SFCLoader] Unsupported: speech history\n");
+
+	fmt::print("INFO [SFCLoader] Loading macros...\n");
+	serialize_macros(*this);
+
+	fmt::print("WARN [SFCLoader] Unsupported: death row objects\n");
+	fmt::print("WARN [SFCLoader] Unsupported: event objects\n");
+	fmt::print("WARN [SFCLoader] Unsupported: current score\n");
+	fmt::print("WARN [SFCLoader] Unsupported: current health\n");
+	fmt::print("WARN [SFCLoader] Unsupported: hatchery eggs\n");
+	fmt::print("WARN [SFCLoader] Unsupported: natural eggs\n");
+	fmt::print("WARN [SFCLoader] Unsupported: dead norns\n");
+	fmt::print("WARN [SFCLoader] Unsupported: live norns\n");
+	fmt::print("WARN [SFCLoader] Unsupported: breeder score\n");
+	fmt::print("WARN [SFCLoader] Unsupported: system tick\n");
+	fmt::print("WARN [SFCLoader] Unsupported: stuffed norns\n");
 }
