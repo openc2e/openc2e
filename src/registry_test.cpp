55671a68787be4d43de73d660c2b67388c370c5b
Michael Maltese <michaeljosephmaltese@gmail.com>
2025-01-08 09:52:41 -0500

    remove unused file src/registry_test.cpp

--- src/registry_test.cpp
+++
@@ -1,8 +0,0 @@
-#include "common/WindowsRegistry.h"
-
-#include <fmt/core.h>
-
-int main() {
-	auto path = registry_get_string_value(REGISTRY_HKEY_LOCAL_MACHINE, "SOFTWARE\\WOW6432Node\\Gameware Development\\Creatures 1\\1.0", "Main Directory");
-	fmt::print("Data: '{}'\n", path);
-}
