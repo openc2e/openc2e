openc2e/openc2e

commit 3fe4cbd624a2d854d83adeb3ddcd10d0f9a99081

Remove all use of <regex>
C++ <regex> is slow to compile and slow to run, and we don't actually
need the full power.

src/PathResolver.cpp
@@ -23,11 +23,11 @@
 #include "utils/ascii_tolower.h"
 #include "utils/case_insensitive_filesystem.h"
 #include "utils/namedifstream.h"
+#include "utils/wildcard_match.h"
 
 #include <assert.h>
 #include <fmt/core.h>
 #include <ghc/filesystem.hpp>
-#include <regex>
 #include <string>
 #include <system_error>
 #include <unordered_map>
@@ -128,25 +128,12 @@ static fs::path findFile(FileDirectory type, fs::path name) {
 }
 
 static std::vector<fs::path> findByWildcard(FileDirectory type, std::string wild) {
-	fs::path::string_type search_pattern = "^";
-	for (auto c : wild) {
-		if (c == '*') {
-			search_pattern += ".*";
-		} else if (c == '?') {
-			search_pattern += ".";
-		} else {
-			search_pattern += std::string("[") + c + "]";
-		}
-	}
-	search_pattern += "$";
-	std::basic_regex<fs::path::value_type> re(search_pattern, std::regex_constants::icase);
-
 	std::vector<fs::path> results;
 	// TODO: check user directory before or after data directories?
 	for (auto d : data_directories) {
 		auto dirname = getDirectory(d, type);
 		for (const auto& entry : case_insensitive_filesystem::directory_iterator(dirname)) {
-			if (std::regex_match(entry.lexically_relative(dirname).native(), re)) {
+			if (wildcard_match(wild, entry.lexically_relative(dirname).native())) {
 				results.push_back(entry);
 			}
 		}

src/utils/wildcard_match.h
@@ -1,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+inline bool wildcard_match_helper(const char* pattern, const char* value) {
+	// TODO: string_view would be nicer than raw char pointers, and allow us to
+	// use only a single function...
+	while (true) {
+		if (*pattern == '\0' && *value == '\0') {
+			return true;
+		}
+		if (*pattern == '*' && *(pattern + 1) != '\0' && *value == '\0') {
+			return false;
+		}
+		if (*pattern == '?' || *pattern == *value) {
+			pattern += 1;
+			value += 1;
+			continue;
+		}
+		if (*pattern == '*') {
+			return wildcard_match_helper(pattern, value + 1) || wildcard_match_helper(pattern + 1, value);
+		}
+		return false;
+	}
+}
+
+inline bool wildcard_match(const std::string& pattern, const std::string& value) {
+	return wildcard_match_helper(pattern.c_str(), value.c_str());
+}

CMakeLists.txt
@@ -333,10 +333,10 @@ include(GoogleTest)
 include(Openc2eOutputOnTestFailure)
 
 add_executable(test_openc2e
-        tests/test_demangle.cpp
 	tests/test_caos.cpp
 	tests/test_encoding.cpp
 	tests/test_pray.cpp
+        tests/test_utils.cpp
 )
 target_link_libraries(test_openc2e openc2e-core gtest gtest_main)
 if(COMMAND gtest_discover_testsp)

src/caosparser.cpp
@@ -139,7 +139,6 @@ static CAOSNodePtr parse_command(CAOSParserState& state, bool is_toplevel) {
 	// find a command from the first token
 	std::string command = ascii_tolower(state.tokens[state.p].value);
 	std::string commandnormalized = command;
-	// TODO: <regex> makes compile time slow
 	if (command.size() == 4 && command[0] == 'o' && command[1] == 'b' && command[2] == 'v' && isdigit(command[3])) {
 		commandnormalized = "obvx";
 	} else if (command.size() == 4 && command[0] == 'v' && command[1] == 'a' && command[2] == 'r' && isdigit(command[3])) {

src/fileformats/Caos2PrayParser.re2c.cpp
@@ -2,10 +2,10 @@
 #include "fileformats/caoslexer.h"
 #include "fileformats/caostoken.h"
 #include "utils/encoding.h"
+#include "utils/iendswith.h"
 #include "utils/overload.h"
 
 #include <cassert>
-#include <regex>
 
 static char unescape(char c) {
 	switch (c) {
@@ -19,10 +19,6 @@ static char unescape(char c) {
 static bool iequals(const std::string& a, const std::string& b) {
 	return std::equal(a.begin(), a.end(), b.begin(), b.end(),
 		[](char a, char b) { return std::tolower(a) == std::tolower(b); });
-}
-
-static bool imatch(const std::string& s, std::string regex) {
-	return std::regex_search(s, std::regex(regex, std::regex::icase));
 }
 
 struct c2ptoken {
@@ -188,19 +184,19 @@ class Caos2PrayParserImpl {
 			"Dependency " + std::to_string(dependency_count), a});
 		std::string categorykey =
 			"Dependency Category " + std::to_string(dependency_count);
-		if (imatch(a, "\\.(mng|wav)$")) {
+		if (iendswith(a, ".mng") || iendswith(a, ".wav")) {
 			events->push_back(IntegerTag{categorykey, 1});
-		} else if (imatch(a, "\\.(c16|s16)$")) {
+		} else if (iendswith(a, ".c16") || iendswith(a, ".s16")) {
 			events->push_back(IntegerTag{categorykey, 2});
-		} else if (imatch(a, "\\.(gen|gno)$")) {
+		} else if (iendswith(a, ".gen") || iendswith(a, ".gno")) {
 			events->push_back(IntegerTag{categorykey, 3});
-		} else if (imatch(a, "\\.att$")) {
+		} else if (iendswith(a, ".att")) {
 			events->push_back(IntegerTag{categorykey, 4});
 		}
 		// skip category 5, which is Overlay Data
-		else if (imatch(a, "\\.blk$")) {
+		else if (iendswith(a, ".blk")) {
 			events->push_back(IntegerTag{categorykey, 6});
-		} else if (imatch(a, "\\.catalogue$")) {
+		} else if (iendswith(a, ".catalogue")) {
 			events->push_back(IntegerTag{categorykey, 7});
 		} else {
 			*events = {Error{"Unknown dependency category for '" + a}};

src/fileformats/attFile.cpp
@@ -19,9 +19,11 @@
 #include "fileformats/attFile.h"
 
 #include <cassert>
-#include <regex>
+#include <cctype>
+#include <string>
 
 std::istream& operator>>(std::istream& i, attFile& f) {
+	// TODO: replace this whole thing with a proper parser
 	f.nolines = 0;
 
 	std::string s;
@@ -36,12 +38,13 @@ std::istream& operator>>(std::istream& i, attFile& f) {
 
 		bool havefirst = false;
 		unsigned int x = 0;
-		const std::regex ws_re("\\s+");
-		for (
-			auto beg = std::sregex_token_iterator(s.begin(), s.end(), ws_re, -1);
-			beg != std::sregex_token_iterator();
-			beg++) {
-			unsigned int val = atoi(beg->str().c_str());
+		size_t p = 0;
+		while (true) {
+			const char* start = s.c_str() + p;
+			while (p < s.size() && std::isdigit(s[p])) {
+				p++;
+			}
+			unsigned int val = atoi(start);
 			if (havefirst) {
 				f.attachments[f.nolines][f.noattachments[f.nolines] * 2] = x;
 				f.attachments[f.nolines][(f.noattachments[f.nolines] * 2) + 1] = val;
@@ -50,6 +53,16 @@ std::istream& operator>>(std::istream& i, attFile& f) {
 			} else {
 				havefirst = true;
 				x = val;
+			}
+			if (p == s.size()) {
+				break;
+			}
+			assert(s[p] == ' ' || s[p] == '\r');
+			while (p < s.size() && s[p] == ' ') {
+				p++;
+			}
+			if (p == s.size() || s[p] == '\r') {
+				break;
 			}
 		}
 

src/utils/iendswith.h
@@ -1,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+inline bool iendswith(const std::string& s, const std::string& pattern) {
+	return std::equal(s.begin() + (s.size() - pattern.size()), s.end(), pattern.begin(), pattern.end(),
+		[](char a, char b) { return std::tolower(a) == std::tolower(b); });
+}

tests/test_demangle.cpp
@@ -1,18 +1,0 @@
-#include <gtest/gtest.h>
-
-#include "utils/demangle.h"
-#include <string>
-#include <typeinfo>
-
-class myexception {};
-
-TEST(demangle, demangle) {
-  myexception e;
-
-  std::string name = typeid(e).name();
-  std::string demangled = demangle(name.c_str());
-
-  EXPECT_TRUE(demangled == "myexception" || demangled == name)
-      << "Actual value: \"" << demangled << "\", expected \"myexception\" or \""
-      << name << "\".";
-}

commit d59e5bb2cf0d76015d8767c428b71ba15f2a678b

bugfix: resolve wildcard paths case-insensitively
Bug introduced in 3fe4cbd624a2d854d83adeb3ddcd10d0f9a99081
which removed use of <regex>.

PathResolver::findByWildcard passed std::regex_constants::icase
to the regex but that was overlooked when converting to the new
wildcard_match function.

src/common/wildcard_match.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#include "Ascii.h"
 #include "StringView.h"
 
-bool wildcard_match(StringView pattern, StringView value) {
+bool wildcard_match_ignore_case(StringView pattern, StringView value) {
 	while (true) {
 		if (pattern.empty() && value.empty()) {
 			return true;
@@ -13,7 +14,7 @@ bool wildcard_match(StringView pattern, StringView value) {
 		if (value.empty()) {
 			return pattern.size() == 1 && pattern[0] == '*';
 		}
-		if (pattern[0] == '?' || pattern[0] == value[0]) {
+		if (pattern[0] == '?' || to_ascii_lowercase(pattern[0]) == to_ascii_lowercase(value[0])) {
 			pattern = pattern.substr(1);
 			value = value.substr(1);
 			continue;
@@ -21,7 +22,7 @@ bool wildcard_match(StringView pattern, StringView value) {
 		if (pattern[0] == '*') {
 			// TODO: this recursive/branching implementation is ugly. can we do it linearly
 			// without using too much extra memory? maybe using a static vector?
-			return wildcard_match(pattern, value.substr(1)) || wildcard_match(pattern.substr(1), value);
+			return wildcard_match_ignore_case(pattern, value.substr(1)) || wildcard_match_ignore_case(pattern.substr(1), value);
 		}
 		return false;
 	}

src/openc2e/PathResolver.cpp
@@ -134,7 +134,7 @@ static std::vector<fs::path> findByWildcard(FileDirectory type, std::string wild
 			wild = fs::path(wild).filename().string();
 		}
 		for (const auto& entry : case_insensitive_filesystem::directory_iterator(dirname)) {
-			if (wildcard_match(wild, entry.lexically_relative(dirname).native())) {
+			if (wildcard_match_ignore_case(wild, entry.lexically_relative(dirname).native())) {
 				results.push_back(entry);
 			}
 		}

src/opencreatures1/PathManager.cpp
@@ -43,7 +43,7 @@ std::vector<fs::path> PathManager::find_path_wildcard(PathType type, const std::
 	// assume pattern is a single filename, cannot have directory separators
 	std::vector<fs::path> results;
 	for (const auto& entry : case_insensitive_filesystem::directory_iterator(dirname)) {
-		if (wildcard_match(pattern, entry.filename().string())) {
+		if (wildcard_match_ignore_case(pattern, entry.filename().string())) {
 			results.push_back(entry.filename());
 		}
 	}

src/common/tests/WildcardTest.cpp
@@ -6,11 +6,16 @@
 
 
 TEST(Wildcard, wildcard_match) {
-	EXPECT_FALSE(wildcard_match("hello", "nope"));
-	EXPECT_TRUE(wildcard_match("hello", "hello"));
-	EXPECT_TRUE(wildcard_match("hel?o", "hello"));
-	EXPECT_TRUE(wildcard_match("h*o", "hello"));
-	EXPECT_TRUE(wildcard_match("h*l*o", "hello"));
-	EXPECT_TRUE(wildcard_match("*.agent", "test.agent"));
-	EXPECT_FALSE(wildcard_match("*.agent", "test.agents"));
+	EXPECT_FALSE(wildcard_match_ignore_case("hello", "nope"));
+	EXPECT_TRUE(wildcard_match_ignore_case("hello", "hello"));
+	EXPECT_TRUE(wildcard_match_ignore_case("hel?o", "hello"));
+	EXPECT_TRUE(wildcard_match_ignore_case("h*o", "hello"));
+	EXPECT_TRUE(wildcard_match_ignore_case("h*l*o", "hello"));
+	EXPECT_TRUE(wildcard_match_ignore_case("*.agent", "test.agent"));
+	EXPECT_FALSE(wildcard_match_ignore_case("*.agent", "test.agents"));
+}
+
+TEST(Wildcard, wildcard_match_case_insensitive) {
+	EXPECT_TRUE(wildcard_match_ignore_case("hello", "HELLO"));
+	EXPECT_TRUE(wildcard_match_ignore_case("h*o", "HELLO"));
 }