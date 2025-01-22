b2c906ff3c51fbb796f7ad754dad0fbe779a518d
Michael Maltese <michaeljosephmaltese@gmail.com>
2025-01-22 10:13:20 -0500

    case_insensitive_filesystem: fix race condition when creating files
    Was causing agent injection and catalogue injection to break. Two scenarios:
    
    - the engine wants to create a new file in a non-existent user data
      folder and then read it. order of operations: create folder (mtime
      set); get canonical name for file (cache update, folder is new);
      write file (mtime set); read file. the read fails if the two mtime
      sets are close enough to happen in one "unit" of filesystem mtime
      (and in fact, ghc_filesystem reports mtime in whole seconds, so this
      is likely).
    
    - the engine wants to create many new files and then read them (as when
      injecting an agent). if they are in the same folder, then order of
      operations: write file A (mtime set); get canonical name for file B
      (cache update, folder mtime changed); write file B (mtime set); read
      file B. similar to above, the read fails if the two mtime sets happen
      in the same "unit" of filesystem mtime.
    
    Solved in two different ways:
    
    - in canonical(), if we find that a file exists in filesystem and it's
      not in our cache, _force_ a cache update. previously, it would only
      do a cache update if the mtimes didn't match, but in this case we
      know for sure that our cache is incorrect!
    
    - in ofstream(), if we create a brand new file, insert an entry directly
      into the cache so we can resolve it immediately. this also serves as
      an optimization so we don't have to reread the directory listing.

--- src/common/case_insensitive_filesystem.cpp
+++ src/common/case_insensitive_filesystem.cpp
@@ -30,18 +30,10 @@ static bool path_startswith(const fs::path& path, fs::path prefix) {
 	return std::equal(prefix.begin(), end, path.begin());
 }
 
-static void update_directory(fs::path dirname) {
+static void force_update_directory(fs::path dirname) {
 	assert(!dirname.empty());
 
-	// if the directory is already cached and matches what's on disk, then do nothing
 	fs::path lcdirname = to_ascii_lowercase(dirname);
-	std::error_code mtime_error;
-	if (s_cache.count(lcdirname)
-		// avoid a check for directory existence by using the non-throwing overload
-		// of fs::last_write_time, which returns file_time_type::min() on errors
-		&& fs::last_write_time(s_cache[lcdirname].realfilename, mtime_error) == s_cache[lcdirname].mtime) {
-		return;
-	}
 
 	// remove existing cache entries
 	fs::path withtrailingslash = lcdirname / "";
@@ -60,7 +52,6 @@ static void update_directory(fs::path dirname) {
 		std::error_code err;
 		dirname = case_insensitive_filesystem::canonical(dirname, err);
 		if (err) {
-			s_cache[lcdirname] = {dirname, fs::file_time_type::min()}; // I guess?
 			return;
 		}
 		iter = fs::directory_iterator(dirname);
@@ -71,6 +62,22 @@ static void update_directory(fs::path dirname) {
 	for (const auto& entry : iter) {
 		s_cache[to_ascii_lowercase(entry.path())] = {entry.path()};
 	}
+}
+
+static void maybe_update_directory(fs::path dirname) {
+	assert(!dirname.empty());
+
+	// if the directory is already cached and matches what's on disk, then do nothing
+	fs::path lcdirname = to_ascii_lowercase(dirname);
+	std::error_code mtime_error;
+	if (s_cache.count(lcdirname)
+		// avoid a check for directory existence by using the non-throwing overload
+		// of fs::last_write_time, which returns file_time_type::min() on errors
+		&& fs::last_write_time(s_cache[lcdirname].realfilename, mtime_error) == s_cache[lcdirname].mtime) {
+		return;
+	}
+
+	force_update_directory(dirname);
 }
 
 fs::path canonical(const fs::path& path, std::error_code& ec) {
@@ -91,8 +98,36 @@ fs::path canonical(const fs::path& path, std::error_code& ec) {
 		return s_cache[lcpath].realfilename;
 	}
 
-	// otherwise, update directory cache and then check if we found it
-	update_directory(abspath.parent_path());
+	// if file exists on filesystem but not in cache, we know we need
+	// to update the directory cache. otherwise, only update the directory
+	// cache only if its mtime has changed.
+	//
+	// scenarios to consider:
+	// - the engine wants to create a new file in a non-existent user data
+	//   folder and then read it. order of operations: create folder (mtime
+	//   set); get canonical name for file (cache update, folder is new);
+	//   write file (mtime set); read file. the read fails if the two mtime
+	//   sets are close enough to happen in one "unit" of filesystem mtime
+	//   (and in fact, ghc_filesystem reports mtime in whole seconds, so this
+	//   is likely).
+	//
+	// - the engine wants to create many new files and then read them (as when
+	//   injecting an agent). if they are in the same folder, then order of
+	//   operations: write file A (mtime set); get canonical name for file B
+	//   (cache update, folder mtime changed); write file B (mtime set); read
+	//   file B. similar to above, the read fails if the two mtime sets happen
+	//   in the same "unit" of filesystem mtime.
+	//
+	// we also try to handle these scenarios in create_file by directly adding
+	// files to the cache, but keep this logic here for defensive purposes.
+
+	if (fs::exists(path)) {
+		force_update_directory(abspath.parent_path());
+	} else {
+		maybe_update_directory(abspath.parent_path());
+	}
+
+	// then check if we found it
 	if (s_cache.count(lcpath)) {
 		return s_cache[lcpath].realfilename;
 	}
@@ -110,7 +145,15 @@ std::ofstream ofstream(const fs::path& path) {
 	std::error_code err;
 	auto canonpath = case_insensitive_filesystem::canonical(path, err);
 	if (err) {
-		return std::ofstream(path, std::ios_base::binary);
+		std::ofstream out(path, std::ios_base::binary);
+		if (!out.fail()) {
+			// insert directly into the cache as an optimization. created
+			// files are often immediately read back by other systems (e.g.
+			// when injecting agents), so this avoids a full directory
+			// iteration and cache update.
+			s_cache[to_ascii_lowercase(path)] = {path};
+		}
+		return out;
 	}
 	return std::ofstream(canonpath, std::ios_base::binary);
 }
@@ -121,8 +164,7 @@ directory_iterator::directory_iterator(const fs::path& dirname) {
 	// for cache comparisons we need the whole path lowercased.
 	lcdirname = to_ascii_lowercase(absdirname);
 
-	// TODO: don't update cached directory contents if they're already fresh enough
-	update_directory(absdirname);
+	maybe_update_directory(absdirname);
 
 	// iterate results from the cache
 	it = s_cache.begin();
