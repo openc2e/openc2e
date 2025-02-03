55bb460706f9d1b23ddc9d4c6e77c227fcede2f9
Michael Maltese <michaeljosephmaltese@gmail.com>
2025-02-03 09:37:14 -0500

    FileIOTest: write test files in a temp directory

--- src/common/io/FileIOTest.cpp
+++ src/common/io/FileIOTest.cpp
@@ -1,7 +1,9 @@
+#include "common/Random.h"
 #include "common/io/FileReader.h"
 #include "common/io/FileWriter.h"
 #include "common/io/IOException.h"
 
+#include <fmt/format.h>
 #include <ghc/filesystem.hpp>
 #include <gtest/gtest.h>
 #include <stdint.h>
@@ -21,27 +23,49 @@ static auto fname() {
 	return ::testing::UnitTest::GetInstance()->current_test_info()->name();
 }
 
-TEST(File, file_write_and_read_to_end) {
+class FileTest : public testing::Test {
+  protected:
+	FileTest() = default;
+	FileTest(const FileTest&) = delete;
+	FileTest& operator=(const FileTest&) = delete;
+
+	void SetUp() {
+		original_path_ = fs::current_path();
+		auto dirname = fs::temp_directory_path() / fmt::format("openc2e-test-{:08x}", rand_uint32());
+		create_directory(dirname);
+		fs::current_path(dirname);
+	}
+
+	void TearDown() {
+		if (!original_path_.empty()) {
+			fs::current_path(original_path_);
+		}
+	}
+
+	fs::path original_path_;
+};
+
+TEST_F(FileTest, file_write_and_read_to_end) {
 	FileWriter(fname()).write("hello world");
 	auto contents = FileReader(fname()).read_to_end();
 	EXPECT_EQ(contents, vec("hello world"));
 }
 
-TEST(File, file_write_overwrites) {
+TEST_F(FileTest, file_write_overwrites) {
 	FileWriter(fname()).write("hello world");
 	FileWriter(fname()).write("ciao mondo");
 	auto contents = FileReader(fname()).read_to_end();
 	EXPECT_EQ(contents, vec("ciao mondo"));
 }
 
-TEST(File, file_write_append) {
+TEST_F(FileTest, file_write_append) {
 	FileWriter(fname()).write("hello");
 	FileWriter(fname(), FileWriter::option_append).write(" world");
 	auto contents = FileReader(fname()).read_to_end();
 	EXPECT_EQ(contents, vec("hello world"));
 }
 
-TEST(File, file_write_read_flush_read) {
+TEST_F(FileTest, file_write_read_flush_read) {
 	FileWriter w(fname());
 	w.write("hello");
 
@@ -52,11 +76,11 @@ TEST(File, file_write_read_flush_read) {
 	EXPECT_TRUE(r.has_data_left());
 }
 
-TEST(File, file_read_nonexistent_errors) {
+TEST_F(FileTest, file_read_nonexistent_errors) {
 	EXPECT_THROW(FileReader f(fname()), IOException);
 }
 
-TEST(File, file_read_parts) {
+TEST_F(FileTest, file_read_parts) {
 	FileWriter(fname()).write("hello world");
 
 	FileReader f(fname());
@@ -72,7 +96,7 @@ TEST(File, file_read_parts) {
 	EXPECT_EQ(part, vec("d"));
 }
 
-TEST(File, file_write_parts) {
+TEST_F(FileTest, file_write_parts) {
 	{
 		FileWriter f(fname());
 		f.write("hello");
@@ -83,7 +107,7 @@ TEST(File, file_write_parts) {
 	EXPECT_EQ(contents, vec("hello world"));
 }
 
-TEST(File, file_seek_absolute) {
+TEST_F(FileTest, file_seek_absolute) {
 	FileWriter(fname()).write("hello world");
 	FileReader f(fname());
 	f.seek_absolute(6);
@@ -91,7 +115,7 @@ TEST(File, file_seek_absolute) {
 	EXPECT_EQ(contents, vec("world"));
 }
 
-TEST(File, file_seek_relative_backward) {
+TEST_F(FileTest, file_seek_relative_backward) {
 	FileWriter(fname()).write("hello world");
 	FileReader f(fname());
 	(void)f.read_to_end();
@@ -100,7 +124,7 @@ TEST(File, file_seek_relative_backward) {
 	EXPECT_EQ(contents, vec("world"));
 }
 
-TEST(File, file_seek_relative_forward) {
+TEST_F(FileTest, file_seek_relative_forward) {
 	FileWriter(fname()).write("hello world");
 	FileReader f(fname());
 	f.seek_relative(6);
@@ -108,7 +132,7 @@ TEST(File, file_seek_relative_forward) {
 	EXPECT_EQ(contents, vec("world"));
 }
 
-TEST(File, file_read_tell) {
+TEST_F(FileTest, file_read_tell) {
 	FileWriter(fname()).write("hello world");
 	FileReader f(fname());
 	std::vector<uint8_t> part(5);
@@ -116,28 +140,28 @@ TEST(File, file_read_tell) {
 	EXPECT_EQ(f.tell(), 5);
 }
 
-TEST(File, file_read_to_end_tell) {
+TEST_F(FileTest, file_read_to_end_tell) {
 	FileWriter(fname()).write("hello world");
 	FileReader f(fname());
 	(void)f.read_to_end();
 	EXPECT_EQ(f.tell(), 11);
 }
 
-TEST(File, file_seek_absolute_tell) {
+TEST_F(FileTest, file_seek_absolute_tell) {
 	FileWriter(fname()).write("hello world");
 	FileReader f(fname());
 	f.seek_absolute(6);
 	EXPECT_EQ(f.tell(), 6);
 }
 
-TEST(File, file_seek_relative_forward_tell) {
+TEST_F(FileTest, file_seek_relative_forward_tell) {
 	FileWriter(fname()).write("hello world");
 	FileReader f(fname());
 	f.seek_relative(6);
 	EXPECT_EQ(f.tell(), 6);
 }
 
-TEST(File, file_seek_relative_backward_tell) {
+TEST_F(FileTest, file_seek_relative_backward_tell) {
 	FileWriter(fname()).write("hello world");
 	FileReader f(fname());
 	(void)f.read_to_end();
@@ -145,14 +169,14 @@ TEST(File, file_seek_relative_backward_tell) {
 	EXPECT_EQ(f.tell(), 6);
 }
 
-TEST(File, file_read_past_end_errors) {
+TEST_F(FileTest, file_read_past_end_errors) {
 	FileWriter(fname()).write("hello");
 	FileReader f(fname());
 	std::vector<uint8_t> contents(11);
 	EXPECT_THROW(f.read(contents), IOException);
 }
 
-TEST(File, file_seek_and_read_past_end_errors) {
+TEST_F(FileTest, file_seek_and_read_past_end_errors) {
 	FileWriter(fname()).write("hello");
 	FileReader f(fname());
 	f.seek_absolute(11);
@@ -160,7 +184,7 @@ TEST(File, file_seek_and_read_past_end_errors) {
 	EXPECT_THROW(f.read(contents), IOException);
 }
 
-TEST(File, file_read_parts_and_has_data_left_true) {
+TEST_F(FileTest, file_read_parts_and_has_data_left_true) {
 	FileWriter(fname()).write("hello world");
 	FileReader f(fname());
 	std::vector<uint8_t> contents(5);
@@ -168,7 +192,7 @@ TEST(File, file_read_parts_and_has_data_left_true) {
 	EXPECT_TRUE(f.has_data_left());
 }
 
-TEST(File, file_read_parts_and_has_data_left_false) {
+TEST_F(FileTest, file_read_parts_and_has_data_left_false) {
 	FileWriter(fname()).write("hello");
 	FileReader f(fname());
 	std::vector<uint8_t> contents(5);
@@ -176,21 +200,21 @@ TEST(File, file_read_parts_and_has_data_left_false) {
 	EXPECT_FALSE(f.has_data_left());
 }
 
-TEST(File, file_read_to_end_and_has_data_left_false) {
+TEST_F(FileTest, file_read_to_end_and_has_data_left_false) {
 	FileWriter(fname()).write("hello");
 	FileReader f(fname());
 	(void)f.read_to_end();
 	EXPECT_FALSE(f.has_data_left());
 }
 
-TEST(File, file_seek_and_has_data_left_false) {
+TEST_F(FileTest, file_seek_and_has_data_left_false) {
 	FileWriter(fname()).write("hello");
 	FileReader f(fname());
 	f.seek_absolute(5);
 	EXPECT_FALSE(f.has_data_left());
 }
 
-TEST(File, file_with_unicode_name) {
+TEST_F(FileTest, file_with_unicode_name) {
 	auto fname = fs::u8path("\xf0\x9f\x99\x82");
 	FileWriter(fname).write("hello");
 	auto contents = FileReader(fname).read_to_end();
